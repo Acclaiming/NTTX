4cac3056cd463125907083d383c7bf8570a25f1b
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-01-30 20:23:37 +0800

    ...

--- src/io/kurumi/nttools/fragments/MainFragment.java
+++ src/io/kurumi/nttools/fragments/MainFragment.java
@@ -143,7 +143,7 @@ public class MainFragment extends Fragment {
                     api.createBlock(id);
                     api.destroyBlock(id);
                     
-                    if (showCache.size() < 20) {
+                    if (showCache.size() < 10) {
                         
                         showCache.add(id);
                         
