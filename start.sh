d4ad643708f6fc07102f3cc2469ad7fb833dcd90
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-01-30 10:12:38 +0800

    .....

--- start.sh
+++ start.sh
@@ -20,4 +20,4 @@ for jar in ./libs/markdown/*.jar;do
  export CLASSPATH=$CLASSPATH:$jar
 done
 
-java -classpath $CLASSPATH io.kurumi.nttools.Launch
+java -classpath $CLASSPATH io.kurumi.nttools.Launcher
