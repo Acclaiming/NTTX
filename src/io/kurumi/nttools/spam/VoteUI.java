25c8598b789a7600a057a12adf6527af87f99edf
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-02-11 13:10:39 +0800

    test

--- src/io/kurumi/nttools/spam/VoteUI.java
+++ src/io/kurumi/nttools/spam/VoteUI.java
@@ -210,13 +210,13 @@ public class VoteUI extends FragmentBase implements TimerTask {
 
         }
 
-        fragment.bot.execute(new EditMessageReplyMarkup("@" + TwitterSpam.VOTE_CHANNEL, vote.vote_message_id).replyMarkup(new ButtonMarkup() {{
+        fragment.bot.execute(new EditMessageReplyMarkup("@" + TwitterSpam.VOTE_CHANNEL, vote.vote_message_id)
+                             .replyMarkup(new ButtonMarkup() {{
 
-                                                                                                                                  newButtonLine("同意 : " + vote.agree.size(), POINT_VOTE_AGREE, vote.id);
-                                                                                                                                  newButtonLine("反对 : " + vote.disagree.size(), POINT_VOTE_DISAGREE, vote.id);
+                                                  newButtonLine("同意 : " + vote.agree.size(), POINT_VOTE_AGREE, vote.id);
+                                                  newButtonLine("反对 : " + vote.disagree.size(), POINT_VOTE_DISAGREE, vote.id);
 
-                                                                                                                              }}.markup()));
-
+                                              }}.markup()));
     }
 
     @Override
@@ -226,7 +226,7 @@ public class VoteUI extends FragmentBase implements TimerTask {
 
             updateVote(fragment, vote);
 
-            if (System.currentTimeMillis() - vote.startTime > 24 * 60 * 60 * 1000) {
+            if (System.currentTimeMillis() - vote.startTime > 0 * 24 * 60 * 60 * 1000) {
 
                 if (vote.agree.size() > vote.disagree.size()) {
 
