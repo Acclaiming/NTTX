0e56ec1a9e77c3b31817fdbb7414de827a859645
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-02-12 20:16:23 +0800

    ...

--- src/io/kurumi/nttools/NTTBot.java
+++ src/io/kurumi/nttools/NTTBot.java
@@ -14,7 +14,7 @@ import io.kurumi.nttools.twitter.TwitterFunc;
 
 public class NTTBot extends MainFragment {
 
-    public boolean debug = true;
+    public boolean debug = false;
     public String debugMsg = "bot 正在紧急维护 请明天再来 （￣～￣）";
     
     public NTTBot(File dataDir) {
