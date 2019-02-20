9ca84bb1844ca0afbb6891e4de74230faf704fe2
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-02-20 14:32:12 +0800

    else

--- src/io/kurumi/ntt/funcs/GroupRepeat.java
+++ src/io/kurumi/ntt/funcs/GroupRepeat.java
@@ -8,79 +8,81 @@ import com.pengrad.telegrambot.request.SendSticker;
 import io.kurumi.ntt.utils.BotLog;
 
 public class GroupRepeat extends Fragment {
- 
+
     public static GroupRepeat INSTANCE = new GroupRepeat();
-    
+
     public HashMap<Long,Msg> last = new HashMap<>();
     public HashMap<Long,Integer> count = new HashMap<>();
-    
+
     @Override
     public boolean onGroupMsg(UserData user, Msg msg, boolean superGroup) {
-        
+
         if (!msg.isCommand() && !user.isBot) {
-            
+
             Msg lastMsg = last.get(msg.chatId());
 
             Integer repeatCount = count.get(msg.chatId());
-            
+
             if (repeatCount == null) repeatCount = 0;
-            
+
             if (lastMsg != null && msg.text() != null && msg.text().equals(lastMsg.text())) {
-                
+
                 repeatCount ++;
-                
+
                 if (repeatCount == 1) {
-                    
+
                     msg.send(msg.text()).exec();
-                    
+
                     repeatCount = 0;
-                    
+
                     BotLog.debug("已处理群 " + msg.message().chat().title() + " 复读 : " + msg.text());
-                    
+
                     last.remove(msg.chatId());
-                    
+
                 } else {
-                    
-                    last.put(msg.chatId(),msg);
-                
-             //   BotLog.debug("已记录群 " + msg.message().chat().title() + " 复读 : " + msg.text());
-              
+
+                    last.put(msg.chatId(), msg);
+
+                    //   BotLog.debug("已记录群 " + msg.message().chat().title() + " 复读 : " + msg.text());
+
                 }
-                
-            }
-            
-            if (lastMsg != null && msg.message().sticker() != null && msg.message().sticker().equals(lastMsg.message().sticker())) {
+
+            } else if (lastMsg != null && msg.message().sticker() != null && msg.message().sticker().equals(lastMsg.message().sticker())) {
 
                 repeatCount ++;
 
                 if (repeatCount == 1) {
 
-                    bot().execute(new SendSticker(msg.chatId(),msg.message().sticker().fileId()));
+                    bot().execute(new SendSticker(msg.chatId(), msg.message().sticker().fileId()));
 
                     repeatCount = 0;
-                    
+
                     BotLog.debug("已处理群 " + msg.message().chat().title() + " 表情包 : 「" + msg.message().sticker().emoji() + " 从 " + msg.message().sticker().setName() + "」");
 
                     last.remove(msg.chatId());
-                    
-                } else {
-                    
-                    last.put(msg.chatId(),msg);
 
-               //     BotLog.debug("已记录群 " + msg.message().chat().title() + " 复读 : " + msg.text());
+                } else {
+
+                    last.put(msg.chatId(), msg);
+
+                    //     BotLog.debug("已记录群 " + msg.message().chat().title() + " 复读 : " + msg.text());
 
                 }
 
+            } else {
+
+                last.put(msg.chatId(), msg);
+
             }
-            
-            
-            count.put(msg.chatId(),repeatCount);
-            
-            
+
+
+            count.put(msg.chatId(), repeatCount);
+
+
         }
-        
+
         return false;
-        
+
     }
-    
+
 }
