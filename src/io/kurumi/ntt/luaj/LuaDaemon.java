23a11a25804766477c65475867539af296b8df1f
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-03-21 11:52:30 +0800

    apply luc to globals

--- src/io/kurumi/ntt/luaj/LuaDaemon.java
+++ src/io/kurumi/ntt/luaj/LuaDaemon.java
@@ -3,6 +3,8 @@ package io.kurumi.ntt.luaj;
 import org.luaj.vm2.*;
 import io.kurumi.ntt.db.*;
 import java.util.*;
+import org.luaj.vm2.compiler.*;
+import org.luaj.vm2.luajc.*;
 
 public class LuaDaemon {
 
@@ -12,10 +14,12 @@ public class LuaDaemon {
 
 		luaj = new Globals();
 
+		LuaC.install(luaj);
+		
 	}
 
 	public LuaValue exec(String lua) {
-
+		
 		return luaj.load(lua).call();
 
 	}
