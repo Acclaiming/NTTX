3d160ef87cf23f78225cea166bc046cb5995f3f1
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-03-26 20:33:32 +0800

    loop save content status

--- src/io/kurumi/ntt/funcs/TwitterArchive.java
+++ src/io/kurumi/ntt/funcs/TwitterArchive.java
@@ -14,104 +14,160 @@ import twitter4j.TwitterException;
 public class TwitterArchive extends Fragment {
 
     public static TwitterArchive INSTANCE = new TwitterArchive();
-    
+
     @Override
     public boolean onMsg(UserData user,Msg msg) {
-        
+
         if (!msg.isCommand()) return false;
-        
+
         switch (msg.command()) {
-            
+
             case "status" : statusArchive(user,msg);break;
-            
+
             default : return false;
-            
+
         }
-        
+
         return true;
-        
+
     }
-    
+
     void statusArchive(UserData user,Msg msg) {
-        
+
         if (msg.params().length != 1) {
-            
+
             msg.send("用法 /status <推文链接|ID>").exec();
-            
+
             return;
-            
+
         }
-        
+
         String input = msg.params()[0];
-        
+
         Long statusId = -1L;
-        
+
         if (NumberUtil.isNumber(input)) {
-            
+
             statusId = NumberUtil.parseLong(input);
-            
+
         } else if (input.contains("twitter.com/")) {
-            
+
             input = StrUtil.subAfter(input,"status/",true);
-            
+
             if (input.contains("?")) {
-                
+
                 input = StrUtil.subBefore(input,"?",false);
-                
+
             }
-            
+
             if (NumberUtil.isNumber(input))  {
-                
+
                 statusId = NumberUtil.parseLong(input);
-                
+
             }
-            
+
         }
-        
+
         if (statusId == -1L) {
-            
+
             msg.send("用法 /status <推文链接|ID>").exec();
-            
+
             return;
-            
+
         }
-        
+
         if (StatusArchive.INSTANCE.exists(statusId)) {
-            
+
             msg.send("存档存在 :)").exec();
-            
+
             msg.send(StatusArchive.INSTANCE.get(statusId).toHtml()).html().exec();
-            
+
         } else if (TAuth.exists(user)) {
-            
+
             msg.send("正在拉取 :)").exec();
-            
+
             Twitter api = TAuth.get(user).createApi();
 
             try {
-                
+
                 Status status = api.showStatus(statusId);
-                
+
                 StatusArchive newStatus = StatusArchive.INSTANCE.getOrNew(statusId);
 
                 newStatus.read(status);
-                
+
+                newStatus.save();
+
+                loopStatus(newStatus,api);
+
                 msg.send("已存档 :)").exec();
-                
+
                 msg.send(newStatus.toHtml()).html().exec();
-              
+
             } catch (TwitterException e) {
-                
+
                 msg.send("存档失败 :( 推文还在吗？是锁推推文吗？").exec();
-                
+
             }
 
         } else {
-            
+
             msg.send("存档不存在 :( 乃没有认证账号 无法通过API读取推文... 请使用 /tauth 认证 ( ⚆ _ ⚆ )").exec();
-            
+
         }
-        
+
     }
-    
+
+    void loopStatus(StatusArchive archive,Twitter api) {
+
+        try {
+
+            if (archive.inReplyToStatusId != -1) {
+
+                if (StatusArchive.INSTANCE.exists(archive.inReplyToStatusId)) {
+
+                    loopStatus(StatusArchive.INSTANCE.get(archive.inReplyToStatusId),api);
+
+                } else {
+
+                    Status status = api.showStatus(archive.inReplyToStatusId);
+
+                    StatusArchive inReplyTo = StatusArchive.INSTANCE.getOrNew(archive.inReplyToStatusId);
+
+                    inReplyTo.read(status);
+
+                    inReplyTo.save();
+
+                    loopStatus(inReplyTo,api);
+
+                }
+
+            }
+
+            if (archive.quotedStatusId != -1) {
+
+                if (StatusArchive.INSTANCE.exists(archive.quotedStatusId)) {
+
+                    loopStatus(StatusArchive.INSTANCE.get(archive.quotedStatusId),api);
+
+                } else {
+
+                    Status status = api.showStatus(archive.quotedStatusId);
+
+                    StatusArchive qupted = StatusArchive.INSTANCE.getOrNew(archive.quotedStatusId);
+
+                    qupted.read(status);
+
+                    qupted.save();
+
+                    loopStatus(qupted,api);
+
+                }
+
+            }
+
+        } catch (TwitterException ex) {}
+
+    }
+
 }
