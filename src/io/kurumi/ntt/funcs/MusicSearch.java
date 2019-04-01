63c55abc91128455750530645ae7cd684eb19d50
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-04-01 10:43:57 +0800

    test cj

---
+++ src/io/kurumi/ntt/funcs/MusicSearch.java
@@ -0,0 +1,48 @@
+package io.kurumi.ntt.funcs;
+
+import io.kurumi.ntt.fragment.Fragment;
+import io.kurumi.ntt.db.UserData;
+import io.kurumi.ntt.model.Msg;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONArray;
+
+public class MusicSearch extends Fragment {
+
+    // String apiUrl = "http://127.0.0.1:11212/";
+    String apiUrlPublic = "https://napi.kurumi.io/";
+
+    boolean usePublicApi = false;
+
+    String searchId(String keywords) {
+
+        HttpResponse resp = HttpUtil.createGet(apiUrlPublic + "/search")
+            .form("keywords",keywords)
+            .execute();
+
+        if (resp.isOk()) {
+
+            JSONObject data = new JSONObject(resp.body());
+            
+            if (data.getByPath("result.songCount",Integer.class) > 0) {
+                
+                JSONArray songs = data.getByPath("result.songs",JSONArray.class);
+                
+                
+            }
+            
+        }
+        
+        return null;
+
+    }
+
+    @Override
+    public boolean onMsg(UserData user,Msg msg) {
+
+
+        return super.onMsg(user,msg);
+    }
+
+}
