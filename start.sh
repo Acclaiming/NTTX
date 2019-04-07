38ec72a288b30e1082cdabd3b2fb23632a9eb2a7
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-04-07 10:39:55 +0800

    :)

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
-git fetch --depth=1 origin master:refs/heads/origin/master
+git fetch -f --depth=1 origin master:refs/heads/origin/master
+
 git update-ref --no-deref HEAD refs/heads/origin/master
+
 git checkout-index -fua && git clean -fdx
 
 for jar in ./libs/*.jar;do
+
  export CLASSPATH=$CLASSPATH:$jar
+ 
 done
 
 find src -name "*.java" > sources.txt && mkdir ./classes
