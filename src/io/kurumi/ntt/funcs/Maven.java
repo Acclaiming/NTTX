dd5304f69c531ddfd4c606c244db9aafe36200c5
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-04-09 17:22:55 +0800

    tesy

--- src/io/kurumi/ntt/funcs/Maven.java
+++ src/io/kurumi/ntt/funcs/Maven.java
@@ -15,6 +15,8 @@ import cn.hutool.core.util.ZipUtil;
 import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.CharsetUtil;
 import io.kurumi.ntt.utils.BotLog;
+import cn.hutool.core.io.IORuntimeException;
+import cn.hutool.core.exceptions.UtilException;
 
 public class Maven extends Fragment {
 
@@ -39,60 +41,54 @@ public class Maven extends Fragment {
 
     void mvn(UserData user,Msg msg) {
 
-        String cmd = "mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get " + 
-            "-DrepoUrl=http://central.maven.org/maven2/ " +
-            "-Dartifact=" + msg.params()[0] +  ":jar " +
-            "-DoutputDirectory=.";
+        try {
+            String cmd = "mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get " + 
+                "-DrepoUrl=http://central.maven.org/maven2/ " +
+                "-Dartifact=" + msg.params()[0] +  ":jar " +
+                "-DoutputDirectory=.";
 
-        msg.send("执行Maven下载...").exec();
+            msg.send("执行Maven下载...").exec();
 
-        String result = RuntimeUtil.execForStr(cmd);
+            Process process = RuntimeUtil.exec(null,Env.CACHE_DIR,cmd);
 
-        File root = new File(".m2/repository");
+            if (process.waitFor() != 0) {}
 
-        List<File> allFiles = FileUtil.loopFiles(root,new FileFilter() {
+            File root = new File(Env.CACHE_DIR,".m2/repository");
 
-                @Override
-                public boolean accept(File file) {
+            List<File> allFiles = FileUtil.loopFiles(root,new FileFilter() {
 
-                    return file.getName().endsWith(".jar");
+                    @Override
+                    public boolean accept(File file) {
 
-                }
-                
-            });
+                        return file.getName().endsWith(".jar");
 
-        if (allFiles.size() == 0) {
+                    }
 
-            msg.send("下载失败...").exec();
-            msg.send(result).exec();
+                });
 
-            return;
+            msg.send("正在合并 " + allFiles.size() + " 个jar...").exec();
 
-        }
+            File cacheDir = new File(Env.CACHE_DIR,"maven");
 
-        msg.send("正在合并 " + allFiles.size() + " 个jar...").exec();
+            for (File jar : allFiles) {
 
-        File cacheDir = new File(Env.CACHE_DIR,"maven/" + UUID.randomUUID().toString());
+                ZipUtil.unzip(jar,cacheDir);
 
-        for (File jar : allFiles) {
+            }
 
-            ZipUtil.unzip(jar,cacheDir);
+            msg.send("正在打包...").exec();
 
-        }
+            File outJar =  ZipUtil.zip(cacheDir);
 
-        msg.send("正在打包...").exec();
+            msg.send("正在发送... 这可能需要几分钟的时间...").exec();
 
+            msg.sendFile(outJar);
 
-        File outJar =  ZipUtil.zip(cacheDir);
-
-
-        msg.send("正在发送... 这可能需要几分钟的时间...").exec();
-
-        msg.sendFile(outJar);
-
-        FileUtil.del(new File(Env.CACHE_DIR,".m2"));
-        FileUtil.del(cacheDir);
-        FileUtil.del(outJar);
+            FileUtil.del(new File(Env.CACHE_DIR,".m2"));
+            FileUtil.del(cacheDir);
+            FileUtil.del(outJar);
+            
+        } catch (InterruptedException e) {}
 
 
     }
