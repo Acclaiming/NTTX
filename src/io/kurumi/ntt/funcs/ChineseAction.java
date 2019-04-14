31580bff8d5a8f5a2422db29ca81bcbc06f29e7a
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-04-14 16:42:31 +0800

    3mmmmm

--- src/io/kurumi/ntt/funcs/ChineseAction.java
+++ src/io/kurumi/ntt/funcs/ChineseAction.java
@@ -9,32 +9,41 @@ import cn.hutool.core.text.*;
 public class ChineseAction extends Fragment {
 
 	public static ChineseAction INSTANCE = new ChineseAction();
-	
+
 	boolean startWithChinese(String msg) {
-		
+
 		if (msg == null) return false;
-		
+
 		char first = msg.charAt(0);
-		
+
 		return first >= 0x4E00 &&  first <= 0x9FA5;
-		
+
 	}
-	
+
 	@Override
 	public boolean onGroupMsg(UserData user,Msg msg,boolean superGroup) {
-		
-		if (msg.replyTo() != null && startWithChinese(msg.command())) {
-			
-			msg.send(user.userName() + " " + msg.command() + "了 " + msg.replyTo().from().userName() + " ！").html().exec();
-			
+
+		if (startWithChinese(msg.command())) {
+
+			if (msg.replyTo() != null) {
+
+				msg.send(user.userName() + " " + msg.command() + "了 " + msg.replyTo().from().userName() + " ~").html().exec();
+
+			} else {
+
+				msg.send(user.userName() + " " + msg.command() + "了 ~").html().exec();
+
+
+			}
+
 			msg.delete();
-			
+
 			return true;
-			
+
 		}
-		
+
 		return false;
-		
+
 	}
-	
+
 }
