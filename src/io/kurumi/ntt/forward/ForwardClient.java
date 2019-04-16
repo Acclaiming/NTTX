cfb20f618c31b2f5630f8720af8069cadefa0c92
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-04-17 06:56:26 +0800

    forward

--- src/io/kurumi/ntt/forward/ForwardClient.java
+++ src/io/kurumi/ntt/forward/ForwardClient.java
@@ -3,17 +3,22 @@ package io.kurumi.ntt.forward;
 import io.kurumi.ntt.fragment.*;
 import io.kurumi.ntt.db.*;
 import io.kurumi.ntt.model.*;
+import io.kurumi.ntt.model.request.*;
 
 public class ForwardClient extends BotFragment {
-    
+
     public Long userId;
     public String botToken;
-    
+
+    public UserData user;
+
     public ForwardClient(Long userId,String botToken)  {
         this.userId = userId;
         this.botToken = botToken;
+
+        this.user = BotDB.getUserData(userId);
     }
-    
+
     @Override
     public String botName() {
         return "ForwardBotClient";
@@ -26,7 +31,35 @@ public class ForwardClient extends BotFragment {
 
     @Override
     public boolean onNPM(UserData user,Msg msg) {
-        msg.forwardTo(userId);
+
+        if (user.id == userId && msg.replyTo() != null && msg.replyTo().from().id.equals(me.id())) {
+            
+            Msg replyTo = msg.replyTo();
+            
+            if (replyTo.message().forwardFromChat() != null) {
+                
+                new Send(this,replyTo.from().id,msg.text()).replyToMessageId(replyTo.message().forwardFromMessageId()).exec();
+                
+                msg.reply("回复成功 ~").exec();
+                
+            }
+
+        }
+        
+        if (!msg.isCommand()) {
+
+            msg.forwardTo(userId);
+
+        } else {
+
+            if ("start".equals(msg.command())) {
+
+                msg.send("这里是 " + user.userName() + " 的私聊BOT ✧٩(ˊωˋ*)و✧ 发送信息给咱就可以了 ~").exec();
+
+            }
+
+        }
+
         return true;
     }
 
