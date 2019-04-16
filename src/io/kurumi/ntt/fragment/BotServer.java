c9d8385ec2980fa9a189f2155fe952b0d56c5841
HiedaNaKan <HiedaNaKan@kurumi.io>
2019-04-17 06:37:33 +0800

    ~

--- src/io/kurumi/ntt/fragment/BotServer.java
+++ src/io/kurumi/ntt/fragment/BotServer.java
@@ -10,38 +10,56 @@ import java.util.*;
 import io.kurumi.ntt.utils.*;
 
 public class BotServer extends NanoHTTPD {
-    
+
     public static HashMap<String,BotFragment> fragments = new HashMap<>();
     public static BotServer INSTANCE;
     public String domain;
-    
+
     public BotServer(int port,String domain) {
-        
+
         super(port);
-        
+
         this.domain = domain;
+
+    }
+
+    public String readBody(IHTTPSession session)  {
+
+        int contentLength = Integer.parseInt(session.getHeaders().get("content-length"));
+        byte[] buf = new byte[contentLength];
+        
+        try {
+            session.getInputStream().read(buf,0,contentLength);
+            
+            return StrUtil.utf8Str(buf);
+            
+        } catch ( IOException e2 ) { }
+        
+        return null;
         
     }
+    
+    
 
     @Override
     public Response serve(IHTTPSession session) {
-        
+
         URL url = URLUtil.url(session.getUri());
 
         BotLog.debug(url.getPath());
-        
+
         String botToken = url.getPath().substring(1);
 
         if (fragments.containsKey(botToken)) {
             
-            fragments.get(botToken).processAsync(BotUtils.parseUpdate(new InputStreamReader(session.getInputStream())));
-            
+            fragments.get(botToken).processAsync(BotUtils.parseUpdate(readBody(session)));
+
         } else {
-            
+
             new TelegramBot(botToken).execute(new DeleteWebhook());
-            
+
         }
-        
+
         return newFixedLengthResponse("");
     }
 
