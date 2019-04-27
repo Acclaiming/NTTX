e0fcb9d6827d01c84eb84a27c33f257b9e980058
root <root@localhost>
2019-04-27 14:01:16 +0800

    ...

--- src/io/kurumi/ntt/funcs/RiceCakeMeme.java
+++ src/io/kurumi/ntt/funcs/RiceCakeMeme.java
@@ -5,30 +5,31 @@ import io.kurumi.ntt.db.UserData;
 import io.kurumi.ntt.model.Msg;
 import cn.hutool.core.util.ReUtil;
 import java.util.List;
+import cn.hutool.core.util.StrUtil;
 
 public class RiceCakeMeme extends Fragment {
 
     public static RiceCakeMeme INSTANCE = new RiceCakeMeme();
-    
+
     @Override
     public boolean onGroup(UserData user,Msg msg) {
 
         if (!msg.hasText() || msg.isCommand()) return false;
 
-        List<String> match = ReUtil.findAllGroup0("羡慕(.*)\\.+",msg.text());
+        if (msg.text().startsWith("羡慕") && msg.text().endsWith("...")) {
 
-        if (match != null && !match.isEmpty()) {
+            if (msg.isReply()) {
 
-            msg.reply("以后把" + match.get(0) + "做成锅包肉吃...").exec();
+                msg.reply("以后把" + msg.replyTo().from().userName() + "做成锅包肉吃...").html().exec();
 
-        } else if (msg.text().matches("羡慕\\.+")) {
+            } else {
+                
+                String content = StrUtil.subBetween(msg.text(),"羡慕","...");
+                
+                msg.reply("以后把" + (content == null || content.isEmpty() ? user.userName() : content) + "做成锅包肉吃...").html().exec();
 
-            msg.reply("以后把你做成锅包肉吃...").exec();
+            }
 
-        } else if (msg.text().contains("三花"))  {
-            
-            msg.reply("以后把你做成三花吃...").exec();
-            
         }
 
         return false;
