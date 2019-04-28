a8cc3f878d8a8d34a97954f593c3ac1e10d2b8a0
root <root@localhost>
2019-04-28 17:06:11 +0800

    add push.sh

---
+++ push.sh
@@ -0,0 +1,15 @@
+git config user.name “稗田 奈间”
+git config user.email “HiedaNaKan@kurumi.io”
+git config credential.helper store
+
+if [ ! $0 ]; then
+
+  git commit -m "一点微小的工作 ~"
+  
+else
+
+  git commit -m $0
+  
+fi
+
+git push origin master
