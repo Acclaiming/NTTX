06a96fa42186c3c5f4451820a0c765552d663b0d
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-05-03 18:29:04 +0800

    再替换一句

--- src/io/kurumi/ntt/utils/TentcentNlp.java
+++ src/io/kurumi/ntt/utils/TentcentNlp.java
@@ -52,6 +52,7 @@ public class TentcentNlp {
             String reply = data.getByPath("data.answer",String.class);
 
             return reply
+                .replace("呵呵，看来心情不错啊","唉嘿 ~")
                 .replace("先让我堵上耳朵，捂上眼睛","噫呜呜噫 ~")
                 .replace("好好说话","好好当个rbq")
                 .replace("不明白你说的什么意思","呜呜呜")
