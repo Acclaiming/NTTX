e5bfac579e070457c61cfacd434279f8bdde3c6f
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-06-01 12:19:01 +0800

    delalll

--- src/io/kurumi/ntt/funcs/admin/DelMsg.java
+++ src/io/kurumi/ntt/funcs/admin/DelMsg.java
@@ -3,13 +3,13 @@ package io.kurumi.ntt.funcs.admin;
 import io.kurumi.ntt.db.*;
 import io.kurumi.ntt.fragment.*;
 import io.kurumi.ntt.model.*;
-import java.security.acl.*;
-import java.util.*;
+import io.kurumi.ntt.model.request.*;
+import com.pengrad.telegrambot.request.*;
 
 public class DelMsg extends Fragment {
 
 	@Override
-	public boolean onGroup(UserData user,Msg msg) {
+	public boolean onMsg(UserData user,Msg msg) {
 		
 		if (!"del".equals(msg.command())) return false;
 		
@@ -20,6 +20,30 @@ public class DelMsg extends Fragment {
 		if (msg.isReply()) {
 			
 			msg.replyTo().delete();
+			
+		}
+		
+		if (msg.params().length > 0) {
+			
+			long chatId = Long.parseLong(msg.params()[0]);
+			
+			Msg send = new Send(chatId,"Test").send();
+
+			if (send != null) {
+				
+				send.delete();
+				
+				Msg status = msg.send("正在删除...").send();
+
+				for (int index = 1;index < send.messageId();index ++) {
+					
+					bot().execute(new DeleteMessage(chatId,send.messageId() - index));
+					
+					status.edit("正在删除 剩余 : " + (send.messageId() - index)).exec();
+					
+				}
+				
+			}
 			
 		}
 		
