eeb336e71b5e260fda2f97c4e8b2fd76145e607b
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-06-06 16:34:59 +0800

    。

--- src/io/kurumi/ntt/utils/FFMpeg.java
+++ src/io/kurumi/ntt/utils/FFMpeg.java
@@ -15,6 +15,8 @@ public class FFMpeg {
 		
 	}
 	
+	static String filters = "scale=320:-1:flags=lanczos";
+	
 	public static File getGifPalettePic(File media) {
 		
 		File cacheFile = new File(Env.CACHE_DIR,"palette_pic/" + media.getName() + ".png");
@@ -23,7 +25,7 @@ public class FFMpeg {
 		
 		try {
 			
-			RuntimeUtil.exec("ffmpeg -i " + media.getPath() + " -b 2048k -r 20 -vf fps=20,scale=320:-1:flags=lanczos,palettegen -y " + cacheFile.getPath()).waitFor();
+			RuntimeUtil.exec("ffmpeg -i " + media.getPath() + " -b 4096k -vf \"" + filters + ",palettegen\" -y " + cacheFile.getPath()).waitFor();
 			
 		} catch (InterruptedException e) {}
 		
@@ -38,7 +40,7 @@ public class FFMpeg {
 		
 		try {
 			
-			return RuntimeUtil.exec("ffmpeg -i " + in.getPath() + " -i " + globalPalettePicPath.getPath() + " -b 2048k -r 20 -lavfi fps=15,scale=270:-1:flags=lanczos[x];[x][1:v]paletteuse -y " + out.getPath()).waitFor() == 0;
+			return RuntimeUtil.exec("ffmpeg -i " + in.getPath() + " -i " + globalPalettePicPath.getPath() + " -b 4096k -lavfi \"" + filters + " [x]; [x][1:v] paletteuse\" -y " + out.getPath()).waitFor() == 0;
 			
 		} catch (InterruptedException e) {
 			
