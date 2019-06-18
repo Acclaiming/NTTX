334eb9e58009bee03f816a9be8fea3fef094a55a
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-06-18 11:57:35 +0800

    喵

--- src/io/kurumi/ntt/db/BotDB.java
+++ src/io/kurumi/ntt/db/BotDB.java
@@ -1,28 +1,14 @@
 package io.kurumi.ntt.db;
 
-import com.mongodb.*;
-import com.mongodb.client.*;
-
-import java.util.*;
-
-import org.bson.codecs.configuration.*;
-import org.bson.codecs.pojo.*;
-import twitter4j.*;
-
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoDatabase;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 
-import static com.mongodb.client.model.Filters.*;
-import static com.mongodb.client.model.Updates.*;
-import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Filters.gt;
-import static com.mongodb.client.model.Filters.not;
-import static com.mongodb.client.model.Updates.combine;
-import static com.mongodb.client.model.Updates.set;
-import static java.util.Arrays.asList;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
-
-import io.kurumi.ntt.db.BotDB.*;
 
 public class BotDB {
 
@@ -37,6 +23,7 @@ public class BotDB {
 
         db = client.getDatabase("NTTools").withCodecRegistry(registry);
 
+        db.listCollectionNames();
 
     }
 
