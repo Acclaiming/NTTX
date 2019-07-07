fe2c425ecd384a8af33ee42d47d0f62fd9a1e9af
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-07-07 09:20:59 +0800

    喵

--- src/io/kurumi/ntt/fragment/ocr/OcrTest.java
+++ src/io/kurumi/ntt/fragment/ocr/OcrTest.java
@@ -10,40 +10,44 @@ public class OcrTest extends Fragment {
 
 	@Override
 	public void init(BotFragment origin) {
-		
+
 		super.init(origin);
-		
+
 		registerFunction("ocr");
 		registerPoint(POINT_OCR);
-		
+
 	}
-	
+
 	final String POINT_OCR = "ocr";
 
 	@Override
 	public void onFunction(UserData user,Msg msg,String function,String[] params) {
-		
+
 		setPrivatePoint(user,POINT_OCR);
-		
+
 		msg.send("现在发送图片").withCancel().exec();
-		
+
 	}
-	
-	EasyOCR ocr = new EasyOCR("/usr/share/tessdata");
+
+	EasyOCR ocr = new EasyOCR(); {
+
+		ocr.setTesseractOptions("--tessdata-dir /usr/share/tessdata -l eng chi_sim chi_tra jpn");
+
+	}
 
 	@Override
 	public void onPoint(UserData user,Msg msg,String point,Object data) {
-		
+
 		if (msg.message().photo() != null) {
-	
+
 			msg.sendTyping();
-			
+
 			msg.send(msg.photo().getPath()).exec();
-			
+
 			msg.send("结果 :",ocr.discern(msg.photo())).exec();
-			
+
 		}
-		
+
 	}
-	
+
 }
