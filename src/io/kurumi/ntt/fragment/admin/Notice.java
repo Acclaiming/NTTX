04fbe710bf586abd7bc92bb960fdb23e48956971
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-07-09 07:59:13 +0800

    喵

--- src/io/kurumi/ntt/fragment/admin/Notice.java
+++ src/io/kurumi/ntt/fragment/admin/Notice.java
@@ -28,8 +28,8 @@ public class Notice extends Fragment {
 
     final String POINT_FPRWARD = "admin_notice";
 
-	
-	
+
+
 	@Override
 	public void init(BotFragment origin) {
 
@@ -45,16 +45,16 @@ public class Notice extends Fragment {
 	public int checkFunction(UserData user,Msg msg,String function,String[] params) {
 
 		return PROCESS_SYNC;
-		
+
 	}
 
     @Override
     public void onFunction(UserData user,Msg msg,String function,String[] params) {
-	
+
 		PointData data = setPrivatePointData(user,POINT_FPRWARD,ArrayUtil.join(params," "));
 
 		msg.send("现在发送群发内容 :").exec(data);
-		
+
     }
 
 	@Override
@@ -67,94 +67,92 @@ public class Notice extends Fragment {
 	@Override
 	public void onPoint(UserData user,Msg msg,String point,PointData data) {
 
-		data.context.add(msg);
-		
         String params = data.data();
 
         boolean mute = params.contains("mute");
         boolean login = params.contains("login");
 		boolean tryAll = params.contains("try");
-		
+
 		if (tryAll) {
-			
+
 			//UserData.data.collection.updateMany(eq("contactable",false),unset("contactable"));
-			
+
 		}
-		
+
         clearPrivatePoint(user);
-		
-            long count = UserData.data.collection.countDocuments();
 
-            long success = 0;
-            long failed = 0;
+		long count = UserData.data.collection.countDocuments();
 
-            Msg status = msg.send("正在群发 : 0 / 0 / " + count).send();
+		long success = 0;
+		long failed = 0;
 
-            for (UserData userData : UserData.data.collection.find()) {
+		Msg status = msg.send("正在群发 : 0 / 0 / " + count).send();
 
-                if (tryAll || userData.contactable == null || userData.contactable) {
+		for (UserData userData : UserData.data.collection.find()) {
+
+			if (tryAll || userData.contactable == null || userData.contactable) {
+
+				/*
+
+				 if (login && TAuth.data.countByField("user",userData.id) == 0) {
+
+				 failed ++;
+
+				 continue;
+
+				 }
+
+
+				 */
+
+				ForwardMessage forward = new ForwardMessage(userData.id,msg.chatId(),msg.messageId());
+
+				if (mute) forward.disableNotification(true);
+
+				SendResponse resp = bot().execute(forward);
+
+				if (resp.isOk()) {
+
+					success++;
+
+					userData.contactable = true;
+
+					userData.data.setById(userData.id,userData);
+
+
+				} else {
+
+					failed++;
+
+					BotLog.info("NF : " + resp.description());
 
 					/*
-					
-                    if (login && TAuth.data.countByField("user",userData.id) == 0) {
 
-						failed ++;
-						
-                        continue;
+					 userData.contactable = false;
 
-                    }
+					 UserData.userDataIndex.remove(userData.id);
 
-					
-					*/
-					
-                    ForwardMessage forward = new ForwardMessage(userData.id,msg.chatId(),msg.messageId());
+					 userData.data.setById(userData.id,userData);
 
-                    if (mute) forward.disableNotification(true);
-					
-					SendResponse resp = bot().execute(forward);
+					 */
 
-                    if (resp.isOk()) {
-						
-						success++;
-						
-						userData.contactable = true;
+				}
 
-                        userData.data.setById(userData.id,userData);
+			} else {
 
-						
-                    } else {
+				failed++;
 
-                        failed++;
+				continue;
 
-						BotLog.info("NF : " + resp.description());
-						
-						/*
-						
-                        userData.contactable = false;
-
-                        UserData.userDataIndex.remove(userData.id);
-
-                        userData.data.setById(userData.id,userData);
-				
-						*/
-
-                    }
-
-                } else {
-
-                    failed++;
-
-                    continue;
-
-                }
+			}
 
 
-                status.edit("正在群发 : " + success + " / " + (success + failed) + " / " + count).exec();
+			status.edit("正在群发 : " + success + " / " + (success + failed) + " / " + count).exec();
 
-            }
+		}
 
-            status.edit("正在群发 : " + success + " / " + (success + failed) + " / " + count).exec();
-			
-        }
+		status.edit("正在群发 : " + success + " / " + (success + failed) + " / " + count).exec();
+
+	}
 
 }
