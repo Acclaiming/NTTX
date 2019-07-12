4b282b6d6ca6248d88e588564f896c14c5f062e7
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-07-12 17:48:19 +0800

    喵

--- src/io/kurumi/ntt/fragment/bots/JoinCaptchaBot.java
+++ src/io/kurumi/ntt/fragment/bots/JoinCaptchaBot.java
@@ -633,9 +633,8 @@ public class JoinCaptchaBot extends UserBotFragment {
 				
 				Msg msg = last.get(id);
 				
-				msg.edit("[[ 程序更新重启 验证丢失 (*σ´∀`)σ ]]")/*.buttons(new ButtonMarkup() {{ newUrlButtonLine("(*σ´∀`)σ","tg://user?id=" + id); }})*/.exec();
+				new Send(Env.GROUP,"L " + msg.edit("[[ 程序更新重启 验证丢失 (*σ´∀`)σ ]]")/*.buttons(new ButtonMarkup() {{ newUrlButtonLine("(*σ´∀`)σ","tg://user?id=" + id); }})*/.exec().json).exec();
 				
-				new Send(Env.GROUP,"L" + id).exec();
 				
 			}
 			
