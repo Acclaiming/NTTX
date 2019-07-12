86ee2cebc98fa7fb59aa701b6ce3e02e4b135144
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-07-12 18:14:39 +0800

    喵

--- src/io/kurumi/ntt/fragment/bots/UserBotFragment.java
+++ src/io/kurumi/ntt/fragment/bots/UserBotFragment.java
@@ -41,15 +41,15 @@ public class UserBotFragment extends BotFragment {
         userId = bot.user;
 
 		params = bot.params;
-		
+
 		List<Long> banned_chat_list = getParam("banned_chat");
 
 		if (banned_chat_list != null) {
-			
+
 			banned_chat = new HashSet<Long>(banned_chat_list);
-			
+
 		}
-		
+
 		if (banned_chat == null) {
 
 			banned_chat = new HashSet<>();
@@ -57,7 +57,7 @@ public class UserBotFragment extends BotFragment {
 			params.put("banned_chat",banned_chat);
 
 		}
-		
+
 		localAdmins.clear();
 		localAdmins.add(userId);
 
@@ -65,11 +65,11 @@ public class UserBotFragment extends BotFragment {
 
 	@Override
 	public void init(BotFragment origin) {
-		
+
 		super.init(origin);
-		
+
 		registerFunction("start");
-		
+
 	}
 
 	public UserData getOwner() {
@@ -89,7 +89,7 @@ public class UserBotFragment extends BotFragment {
 	public String getToken() {
 
 		return botToken;
-		
+
 	}
 
 	public <T> T getParam(String key) {
@@ -118,76 +118,78 @@ public class UserBotFragment extends BotFragment {
 
 	@Override
 	public void onFunction(UserData user,Msg msg,String function,String[] params) {
-
-		if (!msg.isPrivate()) return;
 		
-		onMsg(user,msg);
+		if (msg.isPrivate() && (userId.equals(user.id) || user.admin())) {
 
-		if (!userId.equals(user.id) && !user.admin()) return;
+			if ("start".equals(function)) {
 
-		if ("start".equals(function)) {
+				msg.send(
+					"管理员命令 :\n",
+					"/send <chatId> <text...>",
+					"/edit <chatId> <messageId> <text...>",
+					"/delete <chatId> <messageId>",
+					"/forward <toChatId> <fromChatId> <messageId>",
+					"/export_link <chatid>",
+					"/restrict <chatId> <userId>",
+					"/promote <chatId> <userId>",
+					"/kick <chatId> <userId>",
+					"/unban <chatId> <userId>",
+					"/pin <chatId> <messageId>",
+					"/unpin <chatId>",
+					"/exit <chatId>",
+					"/get_file <fileId>",
+					"/send_file <chatId> <fileId>",
+					"/get_admins <chatId>",
+					"/get_members_count <chatId>",
+					"/get_member <chatId> <userId>",
+					"/ban_chat <chatId>",
+					"/unban_chat <chatId>").exec();
 
-			msg.send(
-				"管理员命令 :\n",
-				"/send <chatId> <text...>",
-				"/edit <chatId> <messageId> <text...>",
-				"/delete <chatId> <messageId>",
-				"/forward <toChatId> <fromChatId> <messageId>",
-				"/export_link <chatid>",
-				"/restrict <chatId> <userId>",
-				"/promote <chatId> <userId>",
-				"/kick <chatId> <userId>",
-				"/unban <chatId> <userId>",
-				"/pin <chatId> <messageId>",
-				"/unpin <chatId>",
-				"/exit <chatId>",
-				"/get_file <fileId>",
-				"/send_file <chatId> <fileId>",
-				"/get_admins <chatId>",
-				"/get_members_count <chatId>",
-				"/get_member <chatId> <userId>",
-				"/ban_chat <chatId>",
-				"/unban_chat <chatId>").exec();
+			} else if ("ban_chat".equals(function)) {
 
-		} else if ("ban_chat".equals(function)) {
+				if (params.length < 1) { invalidParams(msg,"chatId"); return; }
 
-			if (params.length < 1) { invalidParams(msg,"chatId"); return; }
+				long target = NumberUtil.parseLong(params[0]);
 
-			long target = NumberUtil.parseLong(params[0]);
+				if (userId.equals(target)) {
 
-			if (userId.equals(target)) {
+					msg.send("不能屏蔽你自己...").exec();
 
-				msg.send("不能屏蔽你自己...").exec();
+					return;
 
-				return;
+				}
+
+				if (banned_chat.add(target)) {
+
+					msg.send("已屏蔽 如果未退出 请手动退出").exec();
+
+				} else {
+
+					msg.send("已经屏蔽过了...").exec();
+
+				}
+
+			} else if ("unban_chat".equals(function)) {
+
+				if (params.length < 1) { invalidParams(msg,"chatId"); return; }
+
+				if (banned_chat.remove((NumberUtil.parseLong(params[0])))) {
+
+					msg.send("已解除").exec();
+
+				} else {
+
+					msg.send("没有屏蔽过...").exec();
+
+				}
 
 			}
 
-			if (banned_chat.add(target)) {
-
-				msg.send("已屏蔽 如果未退出 请手动退出").exec();
-
-			} else {
-
-				msg.send("已经屏蔽过了...").exec();
-
-			}
-
-		} else if ("unban_chat".equals(function)) {
-
-			if (params.length < 1) { invalidParams(msg,"chatId"); return; }
-
-			if (banned_chat.remove((NumberUtil.parseLong(params[0])))) {
-
-				msg.send("已解除").exec();
-
-			} else {
-
-				msg.send("没有屏蔽过...").exec();
-
-			}
+			return;
 
 		}
+
+		onMsg(user,msg);
 
 	}
 
@@ -209,25 +211,25 @@ public class UserBotFragment extends BotFragment {
 				execute(new LeaveChat(msg.chatId()));
 
 				return PROCESS_REJECT;
-				
+
 			} else if (banned_chat.contains(newMember.id())) {
 
 				execute(new KickChatMember(msg.chatId(),newMember.id().intValue()));
 
 				return PROCESS_REJECT;
-				
+
 			}
 
 		} else if (msg.message().leftChatMember() != null) {
-			
+
 			if (banned_chat.contains(msg.message().leftChatMember().id())) {
-				
+
 				msg.delete();
-				
+
 				return PROCESS_REJECT;
-				
+
 			}
-			
+
 		}
 
 		return PROCESS_ASYNC;
