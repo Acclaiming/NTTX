da7fe87ae387f1b511b2d1ecaab2d40d329f62c0
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-07-14 01:52:08 +0800

    喵

--- src/io/kurumi/ntt/fragment/BotServerHandler.java
+++ src/io/kurumi/ntt/fragment/BotServerHandler.java
@@ -32,241 +32,244 @@ import static io.netty.handler.codec.http.HttpVersion.*;
 
 public class BotServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
 
-	private FullHttpRequest request;
+		private FullHttpRequest request;
 
-	@Override
-	public void channelRead0(ChannelHandlerContext ctx,FullHttpRequest request) throws Exception {
+		@Override
+		public void channelRead0(ChannelHandlerContext ctx,FullHttpRequest request) throws Exception {
 
-		this.request = request;
+				this.request = request;
 
-		if (!request.decoderResult().isSuccess()) {
+				if (!request.decoderResult().isSuccess()) {
 
-			sendError(ctx,BAD_REQUEST);
+						sendError(ctx,BAD_REQUEST);
 
-			return;
-
-		}
-
-		if (request.uri().equals("/data/" + Launcher.INSTANCE.getToken())) {
-
-			File dataFile = new File(Env.CACHE_DIR,"data.zip");
-
-			if (!dataFile.isFile()) {
-
-				sendError(ctx,NOT_FOUND);
-
-				return;
-
-			}
-
-			FullHttpResponse resp = new DefaultFullHttpResponse(HTTP_1_1,OK);
-
-			MimetypesFileTypeMap mimeTypesMap = new MimetypesFileTypeMap();
-
-			resp.headers().set(HttpHeaderNames.CONTENT_TYPE,mimeTypesMap.getContentType(dataFile));
-			resp.headers().set(HttpHeaderNames.CONTENT_LENGTH,dataFile.length());
-
-			final boolean keepAlive = HttpUtil.isKeepAlive(request);
-
-			if (!keepAlive) {
-
-				resp.headers().set(HttpHeaderNames.CONNECTION,HttpHeaderValues.CLOSE);
-
-			} else if (request.protocolVersion().equals(HTTP_1_0)) {
-
-				resp.headers().set(HttpHeaderNames.CONNECTION,HttpHeaderValues.KEEP_ALIVE);
-
-			}
-
-			ctx.write(resp);
-
-			ChannelFuture last = ctx.writeAndFlush(new ChunkedFile(dataFile)).addListener(ChannelFutureListener.CLOSE);
-
-			if (!keepAlive) {
-
-				last.addListener(ChannelFutureListener.CLOSE);
-
-			}
-
-			return;
-
-		} else if (request.uri().equals("/upgrade/" + Launcher.INSTANCE.getToken())) {
-
-			sendOk(ctx);
-
-			new Thread() {
-
-				@Override
-				public void run() {
-
-					new Send(Env.GROUP,"Bot Update Executed : By WebHook").exec();
-
-					try {
-
-						String str = RuntimeUtil.execForStr("bash update.sh");
-
-						new Send(Env.GROUP,str).exec();
-
-						Launcher.INSTANCE.stop();
-						
-						RuntimeUtil.exec("service ntt restart");
-
-					} catch (Exception e) {
-
-						new Send(Env.GROUP,"update failed",BotLog.parseError(e)).exec();
-
-					}
+						return;
 
 				}
 
-			}.start();
+				if (request.uri().equals("/data/" + Launcher.INSTANCE.getToken())) {
 
-			return;
+						File dataFile = new File(Env.CACHE_DIR,"data.zip");
 
-		} else 
+						if (!dataFile.isFile()) {
 
-		if (request.getMethod() != POST || request.uri().length() <= 1) {
+								sendError(ctx,NOT_FOUND);
 
-			sendError(ctx,NOT_FOUND);
+								return;
 
-			return;
+						}
+
+						FullHttpResponse resp = new DefaultFullHttpResponse(HTTP_1_1,OK);
+
+						MimetypesFileTypeMap mimeTypesMap = new MimetypesFileTypeMap();
+
+						resp.headers().set(HttpHeaderNames.CONTENT_TYPE,mimeTypesMap.getContentType(dataFile));
+						resp.headers().set(HttpHeaderNames.CONTENT_LENGTH,dataFile.length());
+
+						final boolean keepAlive = HttpUtil.isKeepAlive(request);
+
+						if (!keepAlive) {
+
+								resp.headers().set(HttpHeaderNames.CONNECTION,HttpHeaderValues.CLOSE);
+
+						} else if (request.protocolVersion().equals(HTTP_1_0)) {
+
+								resp.headers().set(HttpHeaderNames.CONNECTION,HttpHeaderValues.KEEP_ALIVE);
+
+						}
+
+						ctx.write(resp);
+
+						ChannelFuture last = ctx.writeAndFlush(new ChunkedFile(dataFile)).addListener(ChannelFutureListener.CLOSE);
+
+						if (!keepAlive) {
+
+								last.addListener(ChannelFutureListener.CLOSE);
+
+						}
+
+						return;
+
+				} else if (request.uri().equals("/upgrade/" + Launcher.INSTANCE.getToken())) {
+
+						sendOk(ctx);
+
+						new Thread() {
+
+								@Override
+								public void run() {
+
+										new Send(Env.GROUP,"Bot Update Executed : By WebHook").exec();
+
+										try {
+
+												String str = RuntimeUtil.execForStr("bash update.sh");
+
+												new Send(Env.GROUP,str).exec();
+
+												Launcher.INSTANCE.stop();
+
+										} catch (Exception e) {
+
+												new Send(Env.GROUP,"update failed",BotLog.parseError(e)).exec();
+
+										}
+										
+										RuntimeUtil.exec("service ntt restart");
+										
+
+								}
+								
+								
+
+						}.start();
+
+						return;
+
+				} else 
+
+				if (request.getMethod() != POST || request.uri().length() <= 1) {
+
+						sendError(ctx,NOT_FOUND);
+
+						return;
+
+				}
+
+				String botToken = request.uri().substring(1);
+
+				if (!BotServer.fragments.containsKey(botToken)) {
+
+						sendError(ctx,INTERNAL_SERVER_ERROR);
+
+						return;
+
+				}
+
+				try {
+
+						Update update = BotUtils.parseUpdate(request.content().toString(CharsetUtil.CHARSET_UTF_8));
+
+						BotServer.fragments.get(botToken).processAsync(update);
+
+						sendOk(ctx);
+
+				} catch (Exception ex) {
+
+						sendError(ctx,INTERNAL_SERVER_ERROR);
+
+				}
+
+				//update.lock = new ProcessLock();
+
+				/*BaseRequest webhookResponse =*/ 
+				/*
+
+				 if (webhookResponse == null) {
+
+
+
+				 sendOk(ctx); 
+
+				 } else 	{
+
+				 FullHttpResponse resp = new DefaultFullHttpResponse(HTTP_1_1,OK,stringByteBuf(webhookResponse.toWebhookResponse()));
+
+				 resp.headers().set(HttpHeaderNames.CONTENT_TYPE,"application/json; charset=UTF-8");
+
+				 boolean keepAlive = HttpUtil.isKeepAlive(request);
+
+				 if (!keepAlive) {
+
+				 ctx.writeAndFlush(resp).addListener(ChannelFutureListener.CLOSE);
+
+				 } else {
+
+				 resp.headers().set(HttpHeaderNames.CONNECTION,HttpHeaderValues.KEEP_ALIVE);
+
+				 ctx.writeAndFlush(resp);
+
+				 }
+
+				 }
+
+				 */
 
 		}
 
-		String botToken = request.uri().substring(1);
+		@Override
+		public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause) {
 
-		if (!BotServer.fragments.containsKey(botToken)) {
+				cause.printStackTrace();
 
-			sendError(ctx,INTERNAL_SERVER_ERROR);
-			
-			return;
+				if (ctx.channel().isActive()) {
 
-		}
+						sendError(ctx,INTERNAL_SERVER_ERROR);
 
-		try {
-
-			Update update = BotUtils.parseUpdate(request.content().toString(CharsetUtil.CHARSET_UTF_8));
-
-			BotServer.fragments.get(botToken).processAsync(update);
-
-			sendOk(ctx);
-
-		} catch (Exception ex) {
-
-			sendError(ctx,INTERNAL_SERVER_ERROR);
+				}
 
 		}
 
-		//update.lock = new ProcessLock();
+		void sendOk(ChannelHandlerContext ctx) {
 
-		/*BaseRequest webhookResponse =*/ 
-		/*
+				FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1,OK);
 
-		 if (webhookResponse == null) {
-
-
-
-		 sendOk(ctx); 
-
-		 } else 	{
-
-		 FullHttpResponse resp = new DefaultFullHttpResponse(HTTP_1_1,OK,stringByteBuf(webhookResponse.toWebhookResponse()));
-
-		 resp.headers().set(HttpHeaderNames.CONTENT_TYPE,"application/json; charset=UTF-8");
-
-		 boolean keepAlive = HttpUtil.isKeepAlive(request);
-
-		 if (!keepAlive) {
-
-		 ctx.writeAndFlush(resp).addListener(ChannelFutureListener.CLOSE);
-
-		 } else {
-
-		 resp.headers().set(HttpHeaderNames.CONNECTION,HttpHeaderValues.KEEP_ALIVE);
-
-		 ctx.writeAndFlush(resp);
-
-		 }
-
-		 }
-
-		 */
-
-	}
-
-	@Override
-	public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause) {
-
-		cause.printStackTrace();
-
-		if (ctx.channel().isActive()) {
-
-			sendError(ctx,INTERNAL_SERVER_ERROR);
+				this.sendAndCleanupConnection(ctx,response);
 
 		}
 
-	}
 
-	void sendOk(ChannelHandlerContext ctx) {
+		void sendRedirect(ChannelHandlerContext ctx,String newUri) {
 
-		FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1,OK);
-		
-		this.sendAndCleanupConnection(ctx,response);
+				FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1,FOUND);
 
-	}
+				response.headers().set(HttpHeaderNames.LOCATION,newUri);
 
-
-	void sendRedirect(ChannelHandlerContext ctx,String newUri) {
-
-		FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1,FOUND);
-
-		response.headers().set(HttpHeaderNames.LOCATION,newUri);
-
-		this.sendAndCleanupConnection(ctx,response);
-
-	}
-
-	void sendError(ChannelHandlerContext ctx,HttpResponseStatus status) {
-
-		FullHttpResponse response = new DefaultFullHttpResponse(
-			HTTP_1_1,status,Unpooled.copiedBuffer("Failure: " + status + "\r\n",CharsetUtil.CHARSET_UTF_8));
-
-		response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain; charset=UTF-8");
-
-		this.sendAndCleanupConnection(ctx,response);
-
-	}
-
-	void sendAndCleanupConnection(ChannelHandlerContext ctx,FullHttpResponse response) {
-
-		final FullHttpRequest request = this.request;
-
-		final boolean keepAlive = HttpUtil.isKeepAlive(request);
-
-		HttpUtil.setContentLength(response,response.content().readableBytes());
-
-		if (!keepAlive) {
-
-			// We're going to close the connection as soon as the response is sent,
-			// so we should also make it clear for the client.
-
-			response.headers().set(HttpHeaderNames.CONNECTION,HttpHeaderValues.CLOSE);
-
-		} else if (request.protocolVersion().equals(HTTP_1_0)) {
-
-			response.headers().set(HttpHeaderNames.CONNECTION,HttpHeaderValues.KEEP_ALIVE);
+				this.sendAndCleanupConnection(ctx,response);
 
 		}
 
-		ChannelFuture flushPromise = ctx.writeAndFlush(response);
+		void sendError(ChannelHandlerContext ctx,HttpResponseStatus status) {
 
-		if (!keepAlive) {
+				FullHttpResponse response = new DefaultFullHttpResponse(
+						HTTP_1_1,status,Unpooled.copiedBuffer("Failure: " + status + "\r\n",CharsetUtil.CHARSET_UTF_8));
 
-			// Close the connection as soon as the response is sent.
+				response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain; charset=UTF-8");
 
-			flushPromise.addListener(ChannelFutureListener.CLOSE);
+				this.sendAndCleanupConnection(ctx,response);
 
 		}
-	}
+
+		void sendAndCleanupConnection(ChannelHandlerContext ctx,FullHttpResponse response) {
+
+				final FullHttpRequest request = this.request;
+
+				final boolean keepAlive = HttpUtil.isKeepAlive(request);
+
+				HttpUtil.setContentLength(response,response.content().readableBytes());
+
+				if (!keepAlive) {
+
+						// We're going to close the connection as soon as the response is sent,
+						// so we should also make it clear for the client.
+
+						response.headers().set(HttpHeaderNames.CONNECTION,HttpHeaderValues.CLOSE);
+
+				} else if (request.protocolVersion().equals(HTTP_1_0)) {
+
+						response.headers().set(HttpHeaderNames.CONNECTION,HttpHeaderValues.KEEP_ALIVE);
+
+				}
+
+				ChannelFuture flushPromise = ctx.writeAndFlush(response);
+
+				if (!keepAlive) {
+
+						// Close the connection as soon as the response is sent.
+
+						flushPromise.addListener(ChannelFutureListener.CLOSE);
+
+				}
+		}
 
 }
--- src/io/kurumi/ntt/fragment/admin/Control.java
+++ src/io/kurumi/ntt/fragment/admin/Control.java
@@ -12,68 +12,67 @@ import io.kurumi.ntt.Launcher;
 
 public class Control extends Fragment {
 
-	@Override
-	public void init(BotFragment origin) {
-	
-		super.init(origin);
-	
-		registerAdminFunction("stop","upgrade","restart","poweroff","reboot");
+		@Override
+		public void init(BotFragment origin) {
+
+				super.init(origin);
+
+				registerAdminFunction("stop","upgrade","restart","poweroff","reboot");
 
     }
 
     @Override
-    public void onFunction(UserData user, Msg msg, String function, String[] params) {
+    public void onFunction(UserData user,Msg msg,String function,String[] params) {
 
         if ("stop".equals(function)) {
 
             msg.send("Executed : By " + user.userName()).html().exec();
 
-			Launcher.INSTANCE.stop();
-			
+						Launcher.INSTANCE.stop();
+
             RuntimeUtil.exec("service ntt stop");
 
         }  else if ("upgrade".equals(function)) {
 
-			new Send(Env.GROUP, "Bot Update Executed : By " + user.userName()).html().exec();
+						new Send(Env.GROUP,"Bot Update Executed : By " + user.userName()).html().exec();
 
-			 // Launcher.INSTANCE.stop();
-			
+						// Launcher.INSTANCE.stop();
+
             try {
 
-				String str = RuntimeUtil.execForStr("bash update.sh");
+								String str = RuntimeUtil.execForStr("bash update.sh");
 
-				new Send(Env.GROUP, "update successful , now restarting...\n",str).exec();
-				
-				RuntimeUtil.exec("service ntt restart");
-				
-			} catch (Exception e) {
-				
-				new Send(Env.GROUP,"update failed",BotLog.parseError(e)).exec();
-				
-			}
-			
-			
-		} else if ("restart".equals(function)) {
+								new Send(Env.GROUP,"update successful , now restarting...\n",str).exec();
+								
+						} catch (Exception e) {
 
-            new Send(Env.GROUP, "Bot Restart Executed : By " + user.userName()).html().exec();
+								new Send(Env.GROUP,"update failed",BotLog.parseError(e)).exec();
 
-			Launcher.INSTANCE.stop();
-			
+						}
+						
+						RuntimeUtil.exec("service ntt restart");
+						
+				} else if ("restart".equals(function)) {
+
+            new Send(Env.GROUP,"Bot Restart Executed : By " + user.userName()).html().exec();
+
+						Launcher.INSTANCE.stop();
+
             RuntimeUtil.exec("service ntt restart");
 
         } else if ("poweroff".equals(function)) {
 
-			Launcher.INSTANCE.stop();
-			
-            new Send(Env.GROUP, "Server Stop Executed : By " + user.userName()).html().exec();
+						Launcher.INSTANCE.stop();
+
+            new Send(Env.GROUP,"Server Stop Executed : By " + user.userName()).html().exec();
 
             RuntimeUtil.exec("poweroff");
 
         } else if ("reboot".equals(function)) {
 
-			Launcher.INSTANCE.stop();
-			
-            new Send(Env.GROUP, "Bot Restart Executed : By " + user.userName()).html().exec();
+						Launcher.INSTANCE.stop();
+
+            new Send(Env.GROUP,"Bot Restart Executed : By " + user.userName()).html().exec();
 
             RuntimeUtil.exec("reboot");
 
