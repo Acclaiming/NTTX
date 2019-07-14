2daf6e2f165e36a0a2e4e114a3c3572d9bddd64e
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-07-14 13:31:34 +0800

    喵

--- src/io/kurumi/ntt/fragment/group/JoinCaptcha.java
+++ src/io/kurumi/ntt/fragment/group/JoinCaptcha.java
@@ -88,11 +88,11 @@ public class JoinCaptcha extends Fragment {
 								data.waitForCaptcha.add(user.id);
 
 						}
-						
+
 						msg.restrict();
-						
+
 						if (data.passive_mode != null) {
-								
+
 								if (data.delete_service_msg != null) {
 
 										msg.send("你好，新成员 " + newData.userName() + " 为确保群组安全，已将你暂时禁言。请点击下方按钮开始验证。")
@@ -378,19 +378,19 @@ public class JoinCaptcha extends Fragment {
 
 				auth.input = data.require_input != null;
 				auth.code = code;
-				
+
 				if (data.with_image == null) {
 
 						if (auth.input) {
 
 								msg.unrestrict(user.id);
-								
+
 								setGroupPoint(user,POINT_ANSWER,auth);
 
 						} else {
-								
+
 								clearGroupPoint(user);
-								
+
 						}
 
 						if (code.code() == null) {
@@ -445,13 +445,13 @@ public class JoinCaptcha extends Fragment {
 						if (auth.input) {
 
 								msg.unrestrict(user.id);
-								
+
 								setGroupPoint(user,POINT_ANSWER,auth);
-								
+
 						} else {
-								
+
 								clearGroupPoint(user);
-								
+
 						}
 
 						AuthCache old = group.put(user.id,auth);
@@ -466,20 +466,20 @@ public class JoinCaptcha extends Fragment {
 
 
 				}
-				
+
 				auth.task = new TimerTask() {
 
 						@Override
 						public void run() {
-								
+
 								final HashMap<Long, AuthCache> group = cache.containsKey(msg.chatId()) ? cache.get(msg.chatId()) : new HashMap<Long, AuthCache>();
 								final GroupData gd = GroupData.get(msg.chat());
-								
+
 								if (!group.containsKey(user.id)) return;
-								
+
 								auth.serviceMsg.delete();
 								auth.authMsg.delete();
-								
+
 								if (gd.fail_ban == null) {
 
 										gd.waitForCaptcha.remove(user.id);
@@ -493,15 +493,15 @@ public class JoinCaptcha extends Fragment {
 										msg.kick(user.id,true);
 
 								}
-								
-								
+
+
 						}
-						
+
 				};
-				
+
 				if (group.isEmpty()) cache.remove(msg.chatId());
 				else cache.put(msg.chatId(),group);
-				
+
 				BotFragment.mainTimer.schedule(auth.task,new Date(System.currentTimeMillis() + ((data.captcha_time == null ? 50 : data.captcha_time) * 1000)));
 
 		}
@@ -515,10 +515,26 @@ public class JoinCaptcha extends Fragment {
 				final GroupData gd = GroupData.get(msg.chat());
 				final AuthCache auth = (AuthCache)data;
 
-				msg.restrict();
 				msg.delete();
 
-				if (msg.message().newChatMembers() != null) {
+				if (msg.message().leftChatMember() != null) {
+
+						if (auth != null) {
+
+								auth.serviceMsg.delete();
+								auth.task.cancel();
+
+								group.remove(user.id);
+
+						}
+						gd.waitForCaptcha.remove(user.id);
+
+						clearGroupPoint(user);
+
+						return;
+
+
+				} else if (msg.message().newChatMembers() != null) {
 
 						msg.kick(msg.message().newChatMembers()[0].id());
 
@@ -576,46 +592,53 @@ public class JoinCaptcha extends Fragment {
 
 						msg.unrestrict();
 
-				} else if (gd.ft_count != null && (gd.captcha_time == null || (gd.captchaFailed.get(user.id.toString()) + 1 < gd.ft_count))) {
+				} else {
 
-						if (gd.captchaFailed == null) {
+						msg.restrict();
 
-								gd.captchaFailed = new HashMap<>();
+						if (gd.ft_count != null && (gd.captcha_time == null || (gd.captchaFailed.get(user.id.toString()) + 1 < gd.ft_count))) {
 
-								gd.captchaFailed.put(user.id.toString(),1);
 
-						} else if (!gd.captchaFailed.containsKey(user.id.toString())) {
+								if (gd.captchaFailed == null) {
 
-								gd.captchaFailed.put(user.id.toString(),1);
+										gd.captchaFailed = new HashMap<>();
+
+										gd.captchaFailed.put(user.id.toString(),1);
+
+								} else if (!gd.captchaFailed.containsKey(user.id.toString())) {
+
+										gd.captchaFailed.put(user.id.toString(),1);
+
+								} else {
+
+										gd.captchaFailed.put(user.id.toString(),gd.captchaFailed.get(user.id.toString()) + 1);
+
+								}
+
+								msg.send(user.userName() + " , 验证失败。 根据群组设置，你还可以继续重试 :)")
+										.buttons(new ButtonMarkup() {{
+
+														newButtonLine("开始验证",POINT_AUTH,user.id);
+
+														newButtonLine().newButton("通过",POINT_ACC,user.id).newButton("滥权",POINT_REJ,user.id);
+
+												}}).html().exec();
+
+								return;
+
+						} else if (gd.fail_ban == null) {
+
+								gd.waitForCaptcha.remove(user.id);
+
+								msg.kick();
 
 						} else {
 
-								gd.captchaFailed.put(user.id.toString(),gd.captchaFailed.get(user.id.toString()) + 1);
+								gd.waitForCaptcha.remove(user.id);
+
+								msg.kick(user.id,true);
 
 						}
-
-						msg.send(user.userName() + " , 验证失败。 根据群组设置，你还可以继续重试 :)")
-								.buttons(new ButtonMarkup() {{
-
-												newButtonLine("开始验证",POINT_AUTH,user.id);
-
-												newButtonLine().newButton("通过",POINT_ACC,user.id).newButton("滥权",POINT_REJ,user.id);
-
-										}}).html().exec();
-
-					  return;
-
-				} else if (gd.fail_ban == null) {
-
-						gd.waitForCaptcha.remove(user.id);
-
-						msg.kick();
-
-				} else {
-
-						gd.waitForCaptcha.remove(user.id);
-
-						msg.kick(user.id,true);
 
 				}
 
@@ -768,11 +791,11 @@ public class JoinCaptcha extends Fragment {
 								return;
 
 						} else if (NTT.checkGroup(callback)) {
-								
+
 								return;
-								
+
 						}
-						
+
 						if (auth != null) {
 
 								auth.serviceMsg.delete();
@@ -781,9 +804,9 @@ public class JoinCaptcha extends Fragment {
 								group.remove(user.id);
 
 						}
-						
+
 						callback.delete();
-						
+
 						gd.waitForCaptcha.remove(user.id);
 
 						callback.unrestrict(user.id);
@@ -802,7 +825,7 @@ public class JoinCaptcha extends Fragment {
 														newButtonLine().newButton("通过",POINT_ACC,user.id).newButton("滥权",POINT_REJ,user.id);
 
 												}}).html().exec();
-												
+
 								return;
 
 						}
@@ -820,7 +843,7 @@ public class JoinCaptcha extends Fragment {
 
 								callback.unrestrict(user.id);
 
-						} else if (gd.ft_count != null && (gd.captchaFailed == null || ( gd.captchaFailed.get(user.id.toString()) + 1 < gd.ft_count))) {
+						} else if (gd.ft_count != null && (gd.captchaFailed == null || (gd.captchaFailed.get(user.id.toString()) + 1 < gd.ft_count))) {
 
 								if (gd.captchaFailed == null) {
 
@@ -846,8 +869,8 @@ public class JoinCaptcha extends Fragment {
 														newButtonLine().newButton("通过",POINT_ACC,user.id).newButton("滥权",POINT_REJ,user.id);
 
 												}}).html().exec();
-												
-												
+
+
 
 								return;
 
