86ce2a93d87e3eef04251922f548a1ee7d00046f
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-07-17 12:53:47 +0800

    喵

--- src/io/kurumi/ntt/fragment/inline/MakeButtons.java
+++ src/io/kurumi/ntt/fragment/inline/MakeButtons.java
@@ -15,116 +15,116 @@ import java.util.Collections;
 
 public class MakeButtons extends Fragment {
 
-	@Override
-	public void onQuery(UserData user,Query inlineQuery) {
+		@Override
+		public void onQuery(UserData user,Query inlineQuery) {
 
-		if (StrUtil.isBlank(inlineQuery.text)) {
+				if (StrUtil.isBlank(inlineQuery.text)) {
 
-			return;
-			
-		}
+						return;
 
-		String text = inlineQuery.text;
-
-		boolean html = false;
-		boolean buttons = false;
-
-		while (true) {
-
-		 if (text.startsWith(" ") || text.startsWith("\n")) {
-				
-				text = text.substring(1);
-				
-			} else if (text.startsWith("HTML")) {
-
-				text = text.substring(5);
-
-				html = true;
-
-			} else if (text.startsWith("BUTTONS")) {
-
-				text = text.substring(8);
-
-				buttons = true;
-
-			} else {
-
-				break;
-
-			}
-
-		}
-
-		if (StrUtil.isBlank(text) || (!html && !buttons)) {
-			
-			return;
-			
-		}
-
-		ButtonMarkup markup = null;
-
-		if (buttons) {
-
-			for (String line : ArrayUtil.reverse(text.split("\n"))) {
-
-				if (!(line.startsWith("[") && line.endsWith(")"))) break;
-				
-				ButtonLine bL = new ButtonLine();
-				
-				while (line.contains("[")) {
-				
-					String after = StrUtil.subAfter(line,"[",true);
-					line = StrUtil.subBefore(line,"[",true);
-					
-					String bText = StrUtil.subBefore(after,"]",false);
-					String bUrl = StrUtil.subBetween(after,"(",")");
-					
-					if (bText == null || bUrl == null) {
-						
-						// invalid format
-						
-						break;
-						
-					}
-					
-					bL.newUrlButton(bText,bUrl);
-					
 				}
 
-				if (markup == null) markup = new ButtonMarkup();
-				
-				Collections.reverse(bL);
-				
-				markup.add(bL);
-				
-				text = StrUtil.subBefore(text,"\n",true);
-				
-			}
+				String text = inlineQuery.text;
+
+				boolean html = false;
+				boolean buttons = false;
+
+				while (true) {
+
+						if (text.startsWith(" ") || text.startsWith("\n")) {
+
+								text = text.substring(1);
+
+						} else if (text.startsWith("HTML")) {
+
+								text = text.substring(4);
+
+								html = true;
+
+						} else if (text.startsWith("BUTTONS")) {
+
+								text = text.substring(7);
+
+								buttons = true;
+
+						} else {
+
+								break;
+
+						}
+
+				}
+
+				if (StrUtil.isBlank(text) || (!html && !buttons)) {
+
+						return;
+
+				}
+
+				ButtonMarkup markup = null;
+
+				if (buttons) {
+
+						for (String line : ArrayUtil.reverse(text.split("\n"))) {
+
+								if (!(line.startsWith("[") && line.endsWith(")"))) break;
+
+								ButtonLine bL = new ButtonLine();
+
+								while (line.contains("[")) {
+
+										String after = StrUtil.subAfter(line,"[",true);
+										line = StrUtil.subBefore(line,"[",true);
+
+										String bText = StrUtil.subBefore(after,"]",false);
+										String bUrl = StrUtil.subBetween(after,"(",")");
+
+										if (bText == null || bUrl == null) {
+
+												// invalid format
+
+												break;
+
+										}
+
+										bL.newUrlButton(bText,bUrl);
+
+								}
+
+								if (markup == null) markup = new ButtonMarkup();
+
+								Collections.reverse(bL);
+
+								markup.add(bL);
+
+								text = StrUtil.subBefore(text,"\n",true);
+
+						}
+
+				}
+
+				if (markup != null) {
+
+						Collections.reverse(markup);
+
+				}
+
+				ParseMode parseMode = html ? ParseMode.HTML : null;
+
+				inlineQuery.article("完成 *٩(๑´∀`๑)ง*",text,parseMode,markup);
+
+				AnswerInlineQuery request = inlineQuery.reply();
+
+				BaseResponse resp = execute(request);
+
+				if (!resp.isOk()) {
+
+						inlineQuery.article("解析失败","解析失败 : \n\n" + resp.description(),null,null);
+
+						execute(inlineQuery.reply());
+
+				}
 
 		}
-		
-		if (markup != null) {
-			
-			Collections.reverse(markup);
-			
-		}
-		
-		ParseMode parseMode = html ? ParseMode.HTML : null;
-		
-		inlineQuery.article("完成 *٩(๑´∀`๑)ง*",text,parseMode,markup);
-
-		AnswerInlineQuery request = inlineQuery.reply();
-
-		BaseResponse resp = execute(request);
-		
-		if (!resp.isOk()) {
-			
-			inlineQuery.article("解析失败","解析失败 : \n\n" + resp.description(),null,null);
-			
-			execute(inlineQuery.reply());
-			
-		}
-
-	}
 
 }
