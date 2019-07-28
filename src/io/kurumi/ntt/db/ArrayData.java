ec711fc42907669f57e65ba542bd23b902e68aec
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-07-28 17:40:24 +0800

    喵

--- src/io/kurumi/ntt/db/ArrayData.java
+++ src/io/kurumi/ntt/db/ArrayData.java
@@ -3,27 +3,27 @@ package io.kurumi.ntt.db;
 import com.mongodb.client.MongoCollection;
 
 public class ArrayData<T> extends AbsData<T,DataArray<T>> {
-	
+
 	public ArrayData(Class clazz) {
 
-        super(clazz.getSimpleName(), DataArray.class);
+        super(clazz.getSimpleName(),(Class<DataArray<T>>)DataArray.class);
 
     }
 
     public ArrayData(String collectionName) {
 
-		super(collectionName,DataArray.class);
-        
+		super(collectionName,(Class<DataArray<T>>)DataArray.class);
+
     }
 
 	public boolean add(final T data) {
-		
+
 		if (containsId(data)) return false;
-		
+
 		setById(data,new DataArray<T>() {{ id = data; }});
-		
+
 		return true;
-		
+
 	}
-	
+
 }
