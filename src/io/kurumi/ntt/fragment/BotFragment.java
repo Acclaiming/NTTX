fa97a911d6c6d380a53bb844d9af4a1e55e1e330
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-07-30 05:51:52 +0800

    喵

--- src/io/kurumi/ntt/fragment/BotFragment.java
+++ src/io/kurumi/ntt/fragment/BotFragment.java
@@ -248,7 +248,7 @@ public abstract class BotFragment extends Fragment implements UpdatesListener,Ex
 
 			if (data.type == 1) clearPrivatePoint(user).onCancel(user,msg); else clearGroupPoint(user).onCancel(user,msg);
 
-			msg.send("已经取消当前操作 :) ","帮助文档 : @NTT_X").failedWith(9 * 1000);
+			msg.send("已经取消当前操作 :) ",Env.HELP_MESSAGE).failedWith(9 * 1000);
 
 			return;
 
--- src/io/kurumi/ntt/fragment/bots/RssBot.java
+++ src/io/kurumi/ntt/fragment/bots/RssBot.java
@@ -3,23 +3,50 @@ package io.kurumi.ntt.fragment.bots;
 import io.kurumi.ntt.db.*;
 import io.kurumi.ntt.model.*;
 import io.kurumi.ntt.fragment.rss.*;
+import io.kurumi.ntt.utils.Html;
 
 public class RssBot extends UserBotFragment {
 
 	@Override
 	public void reload() {
-	
+
 		super.reload();
-		
+
 		addFragment(new RssSub());
-		
+
+		registerFunction("/start");
+
+	}
+	
+	private static String link = "https://manual.kurumi.io/rss";
+
+	@Override
+	public void onFunction(UserData user,Msg msg,String function,String[] params) {
+
+		if ("start".equals(function)) {
+
+			msg.send("欢迎使用Rss订阅机器人 , 点此 " + Html.a("查看文档",link) + " ~").html().async();
+
+		} else if (function.startsWith("rss_")) {
+			
+			msg.send("在自定义机器人中 " + Html.b("没有 rss_ 前缀") + "！你有认真看 " + Html.a("文档",link) + " 吗？").html().async();
+			
+		} else {
+			
+			msg.send(Html.b("没有这个命令") + " : /" + function,"为什么不看看 " + Html.a("文档",link) + " 呢？").html().async();
+			
+		}
+
+
 	}
 
 	@Override
-	public boolean msg() {
+	public void onFinalMsg(UserData user,Msg msg) {
 
-		return true;
+		msg.send("(？你在干啥 快看看 " + Html.a("文档",link)).html().async();
 
 	}
+
+
 
 }
