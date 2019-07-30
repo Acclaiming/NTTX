d1b7dd3705180cabfa093809d17446ae9095f958
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-07-30 08:55:15 +0800

    喵

--- src/io/kurumi/ntt/fragment/rss/HTMLFilter.java
+++ src/io/kurumi/ntt/fragment/rss/HTMLFilter.java
@@ -99,6 +99,8 @@ public final class HTMLFilter {
 
 	public List<Node> formatTelegraph(String input,String host) {
 
+		BotLog.error(input);
+		
 		String s = input.replaceAll("\n","<br />");
 
 		Matcher m = P_TAGS.matcher(s);
