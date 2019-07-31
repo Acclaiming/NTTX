5f645f3c63ca06c43e943bd62c4028c06c5a012a
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-07-31 15:34:01 +0800

    喵

--- src/io/kurumi/maven/MvnDownloader.java
+++ src/io/kurumi/maven/MvnDownloader.java
@@ -11,45 +11,49 @@ public class MvnDownloader extends Fragment {
 
 	@Override
 	public void init(BotFragment origin) {
-		
+
 		super.init(origin);
-		
+
 		registerFunction("mvn");
-		
+
 	}
 
 	@Override
 	public int checkFunction(UserData user,Msg msg,String function,String[] params) {
-		
+
 		return PROCESS_ASYNC;
-		
+
 	}
 
 	@Override
 	public void onFunction(UserData user,Msg msg,String function,String[] params) {
-		
-		if (params.length < 3) {
-			
-			msg.invalidParams("groupId","artifactId","version").async();
-			
-			return;
-			
-		}
-		
-		MvnResolver resolver = new MvnResolver();
-		
-		MvnArtifact result = resolver.resolve(params[0],params[1],params[2],null);
 
-		if (result == null) {
-			
-			msg.send("无结果").async();
-			
+		if (params.length < 3) {
+
+			msg.invalidParams("groupId","artifactId","version").async();
+
 			return;
-			
+
 		}
-		
+
+		MvnResolver resolver = new MvnResolver();
+
+		MvnArtifact result;
+
+		try {
+
+			result = resolver.resolve(params[0],params[1],params[2],null);
+
+		} catch (MvnException e) {
+
+			msg.send(e.getMessage()).async();
+
+			return;
+
+		}
+
 		msg.send(new JSONObject(new Gson().toJson(result,MvnArtifact.class)).toStringPretty()).async();
-		
+
 	}
-	
+
 }
