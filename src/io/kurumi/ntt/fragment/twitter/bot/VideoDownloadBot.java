51fa9fd371f4a6059ea5d866aea8574fd1f8d282
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-08-04 08:07:40 +0800

    喵

--- src/io/kurumi/ntt/fragment/twitter/bot/VideoDownloadBot.java
+++ src/io/kurumi/ntt/fragment/twitter/bot/VideoDownloadBot.java
@@ -2,18 +2,42 @@ package io.kurumi.ntt.fragment.twitter.bot;
 
 import io.kurumi.ntt.fragment.Fragment;
 import io.kurumi.ntt.fragment.BotFragment;
+import io.kurumi.ntt.db.LongArrayData;
+import io.kurumi.ntt.fragment.twitter.TAuth;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.FilterQuery;
+import twitter4j.TwitterStream;
+import java.util.HashMap;
 
 public class VideoDownloadBot extends Fragment {
 
+	public static LongArrayData data = new LongArrayData("TDBot");
 
+	public static HashMap<Long,TwitterStream> bots = new HashMap<>();
 	
+	public static void startBot(long accountId) {
+
+		TAuth account = TAuth.getById(accountId);
+
+		TwitterStream stream = new TwitterStreamFactory(account.createConfig()).getInstance()
+
+			.addListener(new VideoDownloadListener(account))
+
+			.filter(new FilterQuery()
+			
+					.follow(new long[] {account.id}));
+
+		bots.put(account.id,stream);
+					
+	}
+
 	@Override
 	public void init(BotFragment origin) {
-		
+
 		super.init(origin);
-		
+
 		registerAdminFunction("vdb_init","vdb_unset");
-		
+
 	}
-	
+
 }
---
+++ src/io/kurumi/ntt/fragment/twitter/bot/VideoDownloadListener.java
@@ -0,0 +1,135 @@
+package io.kurumi.ntt.fragment.twitter.bot;
+
+import io.kurumi.ntt.fragment.twitter.TAuth;
+import twitter4j.MediaEntity;
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.UserMentionEntity;
+import twitter4j.StatusUpdate;
+import io.kurumi.ntt.fragment.BotFragment;
+
+public class VideoDownloadListener implements StatusListener {
+
+	private TAuth account;
+	private Twitter api;
+	private String screenName;
+
+	public VideoDownloadListener(TAuth account) {
+
+		this.account = account;
+		this.api = account.createApi();
+		this.screenName = account.archive().screenName;
+
+	}
+
+	void mkReply(Status replyTo,String str) throws TwitterException {
+
+		String reply = "@" + replyTo.getUser().getScreenName() + " ";
+
+		if (replyTo.getUserMentionEntities().length != 0) {
+
+			for (UserMentionEntity mention : replyTo.getUserMentionEntities()) {
+
+				if (!account.id.equals(mention)) {
+
+					reply = reply + "@" + mention.getScreenName() + " ";
+
+				}
+
+			}
+
+		}
+
+		api.updateStatus(new StatusUpdate(reply + " " + str).inReplyToStatusId(replyTo.getId()));
+
+	}
+
+	@Override
+	public void onStatus(final Status status) {
+
+		BotFragment.asyncPool.execute(new Runnable() {
+
+				@Override
+				public void run() {
+					
+					process(status);
+					
+				}
+				
+			});
+		
+	}
+	
+	public void process(Status status) {
+		
+		if (status.getInReplyToStatusId() != -1 && status.getText().contains("@" + screenName)) {
+
+			try {
+
+				Status replyTo = api.showStatus(status.getInReplyToStatusId());
+
+				MediaEntity[] medias = replyTo.getMediaEntities();
+
+				if (medias.length == 0) {
+					
+					mkReply(status,"[媒体下载] 这条推文没有视频/GIF");
+					
+					return;
+					
+				}
+
+				StringBuilder urls = new StringBuilder();
+
+				for (MediaEntity entry : medias) {
+
+					MediaEntity.Variant[] varints = entry.getVideoVariants();
+
+					for (MediaEntity.Variant variant : varints) {
+
+						urls.append("\n").append(variant.getUrl());
+
+					}
+
+				}
+
+				mkReply(status,"[媒体下载] 所有下载链接 :\n" + urls.toString());
+
+
+			} catch (TwitterException e) {
+
+
+
+			}
+
+		}
+
+	}
+
+	@Override
+	public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+	}
+
+	@Override
+	public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+	}
+
+	@Override
+	public void onScrubGeo(long userId,long upToStatusId) {
+	}
+
+	@Override
+	public void onStallWarning(StallWarning warning) {
+	}
+
+	@Override
+	public void onException(Exception ex) {
+	}
+
+
+
+
+}
