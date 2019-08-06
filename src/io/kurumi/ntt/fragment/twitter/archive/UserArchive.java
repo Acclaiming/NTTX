87ca916587ba51e2b6964175ab3d2b3250fea869
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-08-06 09:51:56 +0800

    喵

--- src/io/kurumi/ntt/fragment/twitter/archive/UserArchive.java
+++ src/io/kurumi/ntt/fragment/twitter/archive/UserArchive.java
@@ -212,7 +212,7 @@ public class UserArchive {
         StringBuilder str = new StringBuilder();
         String split = "\n--------------------------------\n";
 
-        String nameL = name;
+        String nameL = name == null ? null : name.replace("\n","");
 
         if (isDisappeared) {
 
@@ -238,7 +238,7 @@ public class UserArchive {
 
         }
 
-        if (!(name = user.getName()).equals(nameL)) {
+        if (!(name = user.getName().replace("\n","")).equals(nameL)) {
 
             str.append(split).append("名称更改 : ").append(nameL).append(" ------> ").append(name);
 
