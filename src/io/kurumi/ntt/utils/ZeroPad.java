ed53dd23dffe5ff00cf2e4f573b3d300d684dae3
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-08-07 11:19:42 +0800

    喵

--- src/io/kurumi/ntt/utils/ZeroPad.java
+++ src/io/kurumi/ntt/utils/ZeroPad.java
@@ -86,7 +86,7 @@ public class ZeroPad {
 
 		}
 		
-		String[] encodedArray = StrUtil.split(encoded,text.length() - 1);
+		String[] encodedArray = StrUtil.split(encoded,text.toCharArray().length - 1);
 
 		StringBuilder result = new StringBuilder();
 		
