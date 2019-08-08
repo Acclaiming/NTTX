6fbbb8627ecd45b26b1e36f541b2e7cc19291e96
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-08-08 11:43:40 +0800

    喵

--- src/io/kurumi/ntt/fragment/mods/ModuleEnv.java
+++ src/io/kurumi/ntt/fragment/mods/ModuleEnv.java
@@ -11,6 +11,7 @@ import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.ScriptableObject;
 import io.kurumi.ntt.model.Msg;
 import io.kurumi.ntt.db.UserData;
+import org.mozilla.javascript.Function;
 
 public class ModuleEnv extends Fragment {
 
@@ -21,37 +22,37 @@ public class ModuleEnv extends Fragment {
 	public static void loadAllEnv() {
 
 	}
-	
+
 	public static ModuleEnv get(Long userId) {
-		
+
 		if (exiting.containsKey(userId)) return null;
-		
+
 		if (envs.containsKey(userId)) {
-			
+
 			return envs.get(userId);
-			
+
 		}
-		
+
 		ModuleEnv mod = new ModuleEnv(userId);
 
 		envs.put(userId,mod);
-		
+
 		return mod;
-		
+
 	}
-	
+
 	public static HashMap<Long,Boolean> exiting = new HashMap<>();
-	
+
 	public static void exitEnv(Long userId) {
-		
+
 		exiting.put(userId,true);
-		
+
 		envs.remove(userId);
-		
+
 		ModuleEnv toExit = envs.get(userId);
 
 		if (toExit != null) toExit.env.ctx.exit();
-		
+
 	}
 
 	public long userId;
@@ -71,7 +72,7 @@ public class ModuleEnv extends Fragment {
 		functions = env.ctx.newObject(env.env);
 
 		env.putConst("__F",functions);
-		
+
 		reLoadModules();
 
 	}
@@ -82,29 +83,55 @@ public class ModuleEnv extends Fragment {
 
 	@Override
 	public void onFunction(UserData user,Msg msg,String function,String[] params) {
-		
+
 		NModule mod = functionIndex.get(function);
-		
+
 		if (mod.error != null) {
-			
-			msg.send("命令 [ /" + function + " ] 所在模块 " + mod.format() +  出错","\n" + BotLog.parseError(mod.error)).async();
-			
+
+			msg.send("命令 [ /" + function + " ] 所在模块 " + mod.format() + " 出错 :(","\n" + BotLog.parseError(mod.error)).async();
+
+			return;
+
+		}
+
+		Object functionObj = functions.get(function,functions);
+
+		if (functionObj == null || !(functionObj instanceof Function)) {
+
+			msg.send("命令 [ /" + function + " ] 所在模块 " + mod.format() + " 没有定义该函数 : " + functionObj).async();
+
+			return;
+
+		}
+
+		Function fn = ((Function)functionObj);
+
+		try {
+
+			fn.call(env.ctx,mod.env,mod.env,new Object[] { user,msg,function,params });
+
+		} catch (Exception ex) {
+
+			mod.error = new ModuleException("方法 : " + function + " 执行中出错\n\n" + BotLog.parseError(ex));
+
+			msg.send("方法执行中出错 : " + BotLog.parseError(ex));
+
 		}
 
 	}
-	
+
 	public void reLoadModules() {
 
 		File[] files = path.listFiles();
 
 		if (files == null || files.length == 0) return;
-		
+
 		for (File modDir : path.listFiles()) {
-			
+
 			NModule mod = Launcher.GSON.fromJson(FileUtil.readUtf8String(new File(modDir,"package.json")),NModule.class);
 
 			mod.modPath = modDir;
-			
+
 			modules.put(mod.id,mod);
 
 		}
@@ -112,17 +139,17 @@ public class ModuleEnv extends Fragment {
 		for (NModule mod : modules.values()) {
 
 			for (String dep : mod.dependencies) {
-				
+
 				if (!modules.containsKey(dep)) {
-					
+
 					mod.error = new ModuleException("模块 " + mod.id + " 缺少依赖 " + dep);
-					
+
 					continue;
-					
+
 				}
-				
+
 			}
-			
+
 			for (String fn : mod.cmds) {
 
 				NModule err = functionIndex.put(fn,mod);
@@ -132,33 +159,33 @@ public class ModuleEnv extends Fragment {
 					mod.error = new ModuleException("模块函数重复 " + fn + " 在 : " + mod.id + " 与 " + err.id);
 
 					continue;
-					
+
 				}
 
 			}
-			
+
 			File mainPath = new File(mod.modPath,mod.main);
-			
+
 			if (!mainPath.exists()) {
-				
+
 				mod.error = new ModuleException("入口文件 " + mod.main + " 不存在");
-				
+
 				continue;
-				
+
 			}
-			
+
 			mod.env = env.ctx.newObject(env.env);
-			
+
 			ScriptableObject.putConstProperty(mod.env,"mod",mod);
-			
+
 			try {
-				
+
 				env.ctx.evaluateReader(mod.env,FileUtil.getUtf8Reader(mainPath),mainPath.getName(),1,null);
-				
+
 			} catch (Exception e) {
-				
+
 				mod.error = new ModuleException("初始化出错 : \n\n" + BotLog.parseError(e));
-				
+
 			}
 
 		}
