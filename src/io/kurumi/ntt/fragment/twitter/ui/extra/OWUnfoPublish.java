05c0be900737cab438977e7a6d21457f954ea8d7
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-08-10 09:21:03 +0800

    喵

--- src/io/kurumi/ntt/fragment/twitter/ui/extra/OWUnfoPublish.java
+++ src/io/kurumi/ntt/fragment/twitter/ui/extra/OWUnfoPublish.java
@@ -150,8 +150,6 @@ public class OWUnfoPublish extends Fragment {
 
 	void oupConfig(UserData user,Callback callback,String[] params,TAuth account) {
 
-		callback.alert("喵");
-		
 		if (params.length == 0) {
 
 			if (account.oup == null) {
