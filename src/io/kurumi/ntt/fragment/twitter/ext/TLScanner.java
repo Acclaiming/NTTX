54ca7ed7788275a207a6df8ee8a64e9758130f0b
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-08-14 22:11:55 +0800

    喵

--- src/io/kurumi/ntt/fragment/twitter/ext/TLScanner.java
+++ src/io/kurumi/ntt/fragment/twitter/ext/TLScanner.java
@@ -5,7 +5,9 @@ import io.kurumi.ntt.db.UserData;
 import io.kurumi.ntt.fragment.BotFragment;
 import io.kurumi.ntt.fragment.Fragment;
 import io.kurumi.ntt.fragment.twitter.TAuth;
+import io.kurumi.ntt.fragment.twitter.archive.UserArchive;
 import io.kurumi.ntt.model.Msg;
+import io.kurumi.ntt.utils.Html;
 import io.kurumi.ntt.utils.NTT;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -17,6 +19,7 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.UserMentionEntity;
+import cn.hutool.core.util.StrUtil;
 
 public class TLScanner extends Fragment {
 
@@ -51,11 +54,11 @@ public class TLScanner extends Fragment {
 			ResponseList<Status> tl = api.getHomeTimeline(new Paging().count(200));
 
 			for (Status status : tl) {
-				
+
 				ids.add(status.getUser().getId());
 
 				if (status.isRetweet()) ids.add(status.getRetweetedStatus().getUser().getId());
-			
+
 				else for (UserMentionEntity m : status.getUserMentionEntities()) ids.add(m.getId());
 
 			}
@@ -67,25 +70,27 @@ public class TLScanner extends Fragment {
 			return;
 
 		}
-		
+
 		if (ids.isEmpty()) {
-			
+
 			stat.edit("时间线上什么也没有....").async();
-			
+
 			return;
-			
+
 		}
-		
+
 		HashSet<Long> target = new HashSet<>();
-		
+
 		Iterator<Long> iter = ids.iterator();
 
+		String blockedBy = "";
+		
 		for (int index = 0;iter.hasNext();index ++) {
-			
+
 			Long userId = iter.next();
-			
+
 			target.add(userId);
-			
+
 			try {
 
 				ResponseList<Status> tl = api.getUserTimeline(userId,new Paging().count(200));
@@ -93,64 +98,87 @@ public class TLScanner extends Fragment {
 				for (Status status : tl) {
 
 					if (status.isRetweet()) target.add(status.getRetweetedStatus().getUser().getId());
-					
+
 					else for (UserMentionEntity m : status.getUserMentionEntities()) target.add(m.getId());
 
 				}
 
 			} catch (TwitterException e) {
 
-				stat.edit(NTT.parseTwitterException(e)).async();
+				if (e.getErrorCode() == 136) {
 
-				return;
+					UserArchive bb = UserArchive.show(api,userId);
+					
+					blockedBy += "\n" + Html.b(bb.name) + " " + Html.a("@" + bb.screenName,bb.url());
+
+				} else {
+
+					stat.edit(NTT.parseTwitterException(e)).async();
+
+					return;
+
+				}
 
 			}
-			
+
 			stat.edit("正在扫描 已发现 " + target.size() + " 项 ( " + (index + 1) + " / " + ids.size() + " )").async();
-			
+
 		}
 
 		stat.edit("扫描完成 已发现 " + target.size() + " 项 正在解析...").async();
-		
+
 		float value = 0;
-		float max = target.size();
 		
+		if (!StrUtil.isEmpty(blockedBy)) {
+			
+			value -= blockedBy.split("\n").length * 3;
+			
+			blockedBy = "在你的圈子里，你被他们屏蔽了，所以这个结果可能不准确 :\n\n" + blockedBy;
+			
+		} else {
+			
+			blockedBy = "很好，你没有被你圈子里的任何人屏蔽。";
+			
+		}
+		
+		float max = target.size();
+
 		while (!target.isEmpty()) {
-			
-			// 互相关注 +2 单向关注 -1 单向被关注 +1 无 0 / size * 2
-			
+
+			// 被屏蔽 -3 互相关注 +2 单向关注 -1 单向被关注 +1 无 0 / size * 2
+
 			iter = target.iterator();
-			
+
 			LinkedList<Long> current = new LinkedList<>();
-			
+
 			for (int index = 0;index < 200;index ++) {
-				
+
 				if (iter.hasNext()) current.add(iter.next());
-				
+
 				else break;
-				
+
 			}
-			
+
 			try {
-				
+
 				ResponseList<Friendship> ships = api.lookupFriendships(ArrayUtil.unWrap(current.toArray(new Long[current.size()])));
 
 				for (Friendship ship : ships) {
-					
+
 					if (ship.isFollowedBy() && ship.isFollowing()) value += 2;
 					else if (ship.isFollowedBy()) value ++;
 					else if (ship.isFollowing()) value --;
-					
+
 				}
-				
+
 			} catch (TwitterException e) {}
-			
+
 			stat.edit("正在解析... " + (max - target.size()) + " / " + max).async();
 
 		}
-		
-		stat.edit("解析完成 你的结果是 : " + ((value / (max * 2)) * 100) + "% 你觉得怎么样呢？").async();
-		
+
+		stat.edit("解析完成 你的结果是 : " + ((value / (max * 2)) * 100) + "%",blockedBy).async();
+
 	}
 
 }
