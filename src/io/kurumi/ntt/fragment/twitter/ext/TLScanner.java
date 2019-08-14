42a46fb393553e7487daa228240828eecb1942b5
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-08-14 23:38:35 +0800

    喵

--- src/io/kurumi/ntt/fragment/twitter/ext/TLScanner.java
+++ src/io/kurumi/ntt/fragment/twitter/ext/TLScanner.java
@@ -95,7 +95,7 @@ public class TLScanner extends Fragment {
 
 			try {
 
-				ResponseList<Status> tl = api.getUserTimeline(userId,new Paging().count(30));
+				ResponseList<Status> tl = api.getUserTimeline(userId,new Paging().count(20));
 
 				for (Status status : tl) {
 
