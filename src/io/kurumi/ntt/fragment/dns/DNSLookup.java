7de1dfee373b08ed8cf806a0651b505d01416541
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-08-16 12:04:24 +0800

    喵

--- src/io/kurumi/ntt/fragment/dns/DNSLookup.java
+++ src/io/kurumi/ntt/fragment/dns/DNSLookup.java
@@ -7,72 +7,93 @@ import io.kurumi.ntt.db.UserData;
 import org.xbill.DNS.Lookup;
 import org.xbill.DNS.TextParseException;
 import org.xbill.DNS.Record;
+import org.xbill.DNS.Type;
+import io.kurumi.ntt.utils.Html;
 
 public class DNSLookup extends Fragment {
 
 	@Override
 	public void init(BotFragment origin) {
-		
+
 		super.init(origin);
-		
+
 		registerFunction("dns");
-		
+
 	}
 
 	@Override
 	public void onFunction(UserData user,Msg msg,String function,String[] params) {
-		
+
 		if (params.length == 0) {
-			
+
 			msg.invalidParams("domain").async();
-			
+
 			return;
-			
+
 		}
-		
+
+		int type = Type.A;
+
+		if (params.length > 1) {
+
+			type = Type.value(params[1]);
+
+			if (type < 0) {
+
+				msg.send("Invalid DNS Type").async();
+
+				return;
+
+			}
+
+		}
+
 		Lookup lookup;
-		
+
 		try {
-			
-			lookup = new Lookup(params[0]);
-			
+
+			lookup = new Lookup(params[0],type);
+
 		} catch (TextParseException e) {
-			
+
 			msg.send("Invalid Domain Name").async();
-			
+
 			return;
-			
+
 		}
-		
+
 		lookup.run();
 
 		if (lookup.getResult() != Lookup.SUCCESSFUL) {
-			
+
 			msg.send(lookup.getErrorString()).async();
-			
+
 			return;
-			
+
 		}
-		
-		String message = "域名 : " + params[0] + " 查询结果 : ";
-		
+
+		String message = "域名 : " + params[0] + " 查询结果 (" + type + ") : ";
+
 		Record[] records = lookup.getAnswers();
-		
+
 		if (records.length == 0) {
-			
+
 			message += "没有记录";
-			
+
 		} else {
-			
+
 			message += "\n";
-			
-			for (Record record : records) message += "\n" + record.toString();
-			
+
+			for (Record record : records) {
+
+				message += "\n" + Type.string(record.getType()) + " " + Html.code(record.rdataToString());
+
+			}
+
 		}
-		
+
 		msg.send(message).async();
-		
 
 	}
-	
+
 }
