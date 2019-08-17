d7ae057221b81bf542c76cb9a111cb209691ac01
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-08-17 12:50:52 +0800

    喵

---
+++ src/io/kurumi/ntt/fragment/qq/CqCodeUtil.java
@@ -0,0 +1,130 @@
+package io.kurumi.ntt.fragment.qq;
+
+import java.util.HashMap;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.NumberUtil;
+
+public class CqCodeUtil {
+	
+	public static HashMap<Integer,String> emojiMap;
+	
+	public static String replaceFace(String message) {
+		
+		String cqFace = "[CQ:face,id=";
+		
+		while (message.contains(cqFace)) {
+			
+			String left = StrUtil.subBefore(message,cqFace,false);
+			
+			message = StrUtil.subAfter(message,cqFace,false);
+			
+			String face = StrUtil.subBefore(message,"]",false);
+			
+			face = emojiMap.get(NumberUtil.parseInt(face));
+			
+			message = left + face + StrUtil.subAfter(message,"]",false);
+			
+		}
+		
+		return message;
+		
+	}
+	
+	static {
+		
+		emojiMap.put(0,"\u0001F62E");
+		emojiMap.put(1,"\u0001F623");
+		emojiMap.put(2,"\u0001F60D");
+		emojiMap.put(3,"\u0001F633");
+		emojiMap.put(4,"\u0001F60E");
+		emojiMap.put(5,"\u0001F62D");
+		emojiMap.put(6,"\u0000263A");
+		emojiMap.put(7,"\u0001F637");
+		emojiMap.put(8,"\u0001F634");
+		emojiMap.put(9,"\u0001F62D");
+		emojiMap.put(10,"\u0001F630");
+		emojiMap.put(11,"\u0001F621");
+		emojiMap.put(12,"\u0001F61D");
+		emojiMap.put(13,"\u0001F603");
+		emojiMap.put(14,"\u0001F642");
+		emojiMap.put(15,"\u0001F641");
+		emojiMap.put(16,"\u0001F913");
+		emojiMap.put(18,"\u0001F624");
+		emojiMap.put(19,"\u0001F628");
+		emojiMap.put(20,"\u0001F60F");
+		emojiMap.put(21,"\u0001F60A");
+		emojiMap.put(22,"\u0001F644");
+		emojiMap.put(23,"\u0001F615");
+		emojiMap.put(24,"\u0001F924");
+		emojiMap.put(25,"\u0001F62A");
+		emojiMap.put(26,"\u0001F628");
+		emojiMap.put(27,"\u0001F613");
+		emojiMap.put(28,"\u0001F62C");
+		emojiMap.put(29,"\u0001F911");
+		emojiMap.put(30,"\u0001F44A");
+		emojiMap.put(31,"\u0001F624");
+		emojiMap.put(32,"\u0001F914");
+		emojiMap.put(33,"\u0001F910");
+		emojiMap.put(34,"\u0001F635");
+		emojiMap.put(35,"\u0001F629");
+		emojiMap.put(36,"\u0001F47F");
+		emojiMap.put(37,"\u0001F480");
+		emojiMap.put(38,"\u0001F915");
+		emojiMap.put(39,"\u0001F44B");
+		emojiMap.put(50,"\u0001F641");
+		emojiMap.put(51,"\u0001F913");
+		emojiMap.put(53,"\u0001F624");
+		emojiMap.put(54,"\u0001F92E");
+		emojiMap.put(55,"\u0001F628");
+		emojiMap.put(56,"\u0001F613");
+		emojiMap.put(57,"\u0001F62C");
+		emojiMap.put(58,"\u0001F911");
+		emojiMap.put(73,"\u0001F60F");
+		emojiMap.put(74,"\u0001F60A");
+		emojiMap.put(75,"\u0001F644");
+		emojiMap.put(76,"\u0001F615");
+		emojiMap.put(77,"\u0001F924");
+		emojiMap.put(78,"\u0001F62A");
+		emojiMap.put(79,"\u0001F44A");
+		emojiMap.put(80,"\u0001F624");
+		emojiMap.put(81,"\u0001F914");
+		emojiMap.put(82,"\u0001F910");
+		emojiMap.put(83,"\u0001F635");
+		emojiMap.put(84,"\u0001F629");
+		emojiMap.put(85,"\u0001F47F");
+		emojiMap.put(86,"\u0001F480");
+		emojiMap.put(87,"\u0001F915");
+		emojiMap.put(88,"\u0001F44B");
+		emojiMap.put(96,"\u0001F630");
+		emojiMap.put(97,"\u0001F605");
+		emojiMap.put(98,"\u0001F925");
+		emojiMap.put(99,"\u0001F44F");
+		emojiMap.put(100,"\u0001F922");
+		emojiMap.put(101,"\u0001F62C");
+		emojiMap.put(102,"\u0001F610");
+		emojiMap.put(103,"\u0001F610");
+		emojiMap.put(104,"\u0001F629");
+		emojiMap.put(105,"\u0001F620");
+		emojiMap.put(106,"\u0001F61E");
+		emojiMap.put(107,"\u0001F61F");
+		emojiMap.put(108,"\u0001F60F");
+		emojiMap.put(109,"\u0001F619");
+		emojiMap.put(110,"\u0001F627");
+		emojiMap.put(111,"\u0001F920");
+		emojiMap.put(172,"\u0001F61C");
+		emojiMap.put(173,"\u0001F62D");
+		emojiMap.put(174,"\u0001F636");
+		emojiMap.put(175,"\u0001F609");
+		emojiMap.put(176,"\u0001F913");
+		emojiMap.put(177,"\u0001F635");
+		emojiMap.put(178,"\u0001F61C");
+		emojiMap.put(179,"\u0001F4A9");
+		emojiMap.put(180,"\u0001F633");
+		emojiMap.put(181,"\u0001F913");
+		emojiMap.put(182,"\u0001F602");
+		emojiMap.put(183,"\u0001F913");
+		emojiMap.put(212,"\u0001F633");
+		
+	}
+	
+}
