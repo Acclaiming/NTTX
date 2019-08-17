15d77739ce87b2b74f82331c75442a1efa3262e7
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-08-17 10:38:23 +0800

    喵

--- coolq.sh
+++ coolq.sh
@@ -3,10 +3,10 @@ docker run -ti --rm --name ntt \
              -p 9000:9000 \
              -p 5700:5700 \
              -p 5701:5701 \
-             -e CQHTTP_HOST=127.0.0.1 \
+             -e CQHTTP_HOST=0.0.0.0 \
              -e CQHTTP_PORT=5700 \
              -e CQHTTP_USE_HTTP=true \
-             -e CQHTTP_WS_HOST=127.0.0.1 \
+             -e CQHTTP_WS_HOST=0.0.0.0 \
              -e CQHTTP_WS_PORT=5701 \
              -e CQHTTP_USE_WS=true \
              -e CQHTTP_CONVERT_UNICODE_EMOJI=true \
