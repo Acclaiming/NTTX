344a0cf4cc8a9692b1d04e96d623069eae8ee14c
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-08-20 17:33:04 +0800

    喵

--- src/io/kurumi/ntt/fragment/sorry/MakeGif.java
+++ src/io/kurumi/ntt/fragment/sorry/MakeGif.java
@@ -30,7 +30,7 @@ public class MakeGif extends Fragment {
 
 		setPrivatePoint(user,msg,POINT_MAKE_GIF);
 
-		msg.send("请选择模板 :").withCancel().keyboardVertical(SorryApi.templates.keySet().toArray(new String[SorryApi.templates.size()])).async();
+		msg.send("请选择模板 :").withCancel().keyboardVertical(SorryApi.templates.keySet().toArray()).async();
 
 	}
 
