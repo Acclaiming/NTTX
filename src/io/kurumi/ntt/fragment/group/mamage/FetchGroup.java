c23bb78cbe96e5f402d83cc583e9a64200611be3
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-08-24 15:00:17 +0800

    喵

--- src/io/kurumi/ntt/fragment/group/mamage/FetchGroup.java
+++ src/io/kurumi/ntt/fragment/group/mamage/FetchGroup.java
@@ -7,6 +7,9 @@ import io.kurumi.ntt.fragment.Fragment;
 import io.kurumi.ntt.model.Msg;
 import io.kurumi.ntt.Launcher;
 import java.util.LinkedList;
+import com.pengrad.telegrambot.request.GetChat;
+import com.pengrad.telegrambot.response.GetChatResponse;
+import com.pengrad.telegrambot.request.LeaveChat;
 
 public class FetchGroup extends Fragment {
 
@@ -30,9 +33,29 @@ public class FetchGroup extends Fragment {
 
 			for (GroupData data : GroupData.data.getAll()) {
 
+				if (data.id > 0) {
+					
+					execute(new LeaveChat(data.id));
+					
+					GroupData.data.deleteById(data.id);
+					
+					return;
+					
+				}
+				
 				if (data.last != null) continue;
 				
-				
+				GetChatResponse chatR = Launcher.INSTANCE.execute(new GetChat(data.id));
+
+				if (chatR.isOk()) {
+					
+					GroupData.get(Launcher.INSTANCE,chatR.chat());
+					
+				} else {
+					
+					failed.add(data.id);
+					
+				}
 				
 			}
 
