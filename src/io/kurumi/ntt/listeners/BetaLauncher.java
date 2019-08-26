0627999cb755636d2e57bdf45aea41f2ba1a33de
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-08-26 14:36:05 +0800

    喵

--- src/io/kurumi/ntt/listeners/BetaLauncher.java
+++ src/io/kurumi/ntt/listeners/BetaLauncher.java
@@ -5,6 +5,7 @@ import io.kurumi.ntt.Env;
 import io.kurumi.ntt.td.TdApi.*;
 import io.kurumi.ntt.td.client.TdBot;
 import io.kurumi.ntt.td.model.TdMessage;
+import cn.hutool.log.StaticLog;
 
 public class BetaLauncher extends TdBot {
 
@@ -16,19 +17,21 @@ public class BetaLauncher extends TdBot {
 
 	@Override
 	public void onNewMessage(UpdateNewMessage update) {
-		
+
+		StaticLog.debug("new message {}",update);
+
 		TdMessage msg = new TdMessage(this,update);
 
 		send(msg.send(plainText("喵")));
-		
+
 	}
-	
+
 	InputMessageText plainText(String text) {
-		
+
 		FormattedText format = new FormattedText(text,new TextEntity[0]);
-		
+
 		return new InputMessageText(format,true,true);
-		
+
 	}
 
 }
