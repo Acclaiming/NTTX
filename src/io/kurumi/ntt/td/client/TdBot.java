de5e50071da68601647bb7c1585f8eddaa6af4c4
稗田 奈间 <HiedaNaKan@kurumi.io>
2019-09-05 19:14:48 +0800

    喵

--- src/io/kurumi/ntt/td/client/TdBot.java
+++ src/io/kurumi/ntt/td/client/TdBot.java
@@ -9,35 +9,32 @@ public class TdBot extends TdClient {
 	private String botToken;
 
 	public TdBot(String botToken) {
-		
-		this(botToken,
-		new TdOptions()
-		.databaseDirectory(Env.CACHE_DIR.getPath() + "/td/" + StrUtil.subBefore(botToken,":",false))
-			 .filesDirectory(Env.CACHE_DIR.getPath() + "/td/" + StrUtil.subBefore(botToken,":",false) + "/files"));
-		
+
+		this(botToken,new TdOptions().databaseDirectory(Env.CACHE_DIR.getPath() + "/td/" + StrUtil.subBefore(botToken,":",false)));
+			 
 	}
-	
+
 	public TdBot(String botToken,TdOptions options) {
-		
+
 		super(options);
-		
+
 		this.botToken = botToken;
-		
+
 	}
-	
+
 	@Override
 	public void onAuthorizationState(TdApi.UpdateAuthorizationState state) {
-	
+
 		super.onAuthorizationState(state);
-		
+
 		TdApi.AuthorizationState authState = state.authorizationState;
-		
+
 		if (authState instanceof TdApi.AuthorizationStateWaitPhoneNumber) {
-			
+
 			send(new TdApi.CheckAuthenticationBotToken(botToken));
-			
+
 		}
-		
+
 	}
-	
+
 }
